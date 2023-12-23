3409da0ea0a2f904426506474396beb8c013adfa
MRxwr <nasserhatab@gmail.com>
2023-12-23 05:21:00 +0300

    fix

--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,5 @@
 <header class="sticky-top bg-white border">
-    <div class="row m-0 w-100">
+    <div class="row m-0 pt-1 w-100">
         <div class="col-12 p-0" style="text-align-last: center;">
             <h2 class="text-left"><img src="img/mainLogo.webp" class="logo" alt="logo"></h2>
         </div>
