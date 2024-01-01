541d0f44246d38c97df139154982bc919474992a
MRxwr <nasserhatab@gmail.com>
2024-01-01 03:48:55 +0300

    Update content.php

--- templates2/content.php
+++ templates2/content.php
@@ -3,7 +3,7 @@ include('simple_html_dom.php');
 $curl = curl_init($website2);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3');
-$html = curl_exec($curl);
+echo $html = curl_exec($curl);
 curl_close($curl);
 $dom = str_get_html($html);
 
