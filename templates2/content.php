a3fe0e81a9cf8fba9bb87186500082c2c8b26cf1
MRxwr <nasserhatab@gmail.com>
2024-01-01 03:56:12 +0300

    Update content.php

--- templates2/content.php
+++ templates2/content.php
@@ -3,8 +3,7 @@ include('simple_html_dom.php');
 $curl = curl_init($website2);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3');
-curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1');
-curl_setopt($curl, CURLOPT_COOKIE, 'MicrosoftApplicationsTelemetryFirstLaunchTime=2023-12-30T12:31:00.279Z');
+curl_setopt($curl, CURLOPT_ENCODING, ''); // Enable automatic content decoding
 $html = curl_exec($curl);
 
 $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
