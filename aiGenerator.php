cf1515ad48d961d1360684cbdda1bf84dfd07631
MRxwr <nasserhatab@gmail.com>
2024-05-31 03:14:03 +0300

    Create aiGenerator.php

---
+++ aiGenerator.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+    <title>Ai Generator</title>
+  </head>
+  <body>
+    <h1>Try ai prompts</h1>
+    <div class="container-fluid m-0 p-3">
+        <div class="row m-0 text-center">
+            <div class="col-8"><input name="prompt" id="prompt" value="" class="form-control"></div>
+            <div class="col-4"><button id="submitBtn" class="btn btn-primary">Send</button></div>
+            <div class="col-12"><iframe style="width: 100vh;height: 100vh;" id="frame" src=""></iframe></div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+    <script>
+        document.getElementById("submitBtn").addEventListener("click", function() {
+            var prompt = document.getElementById("prompt").value;
+            var frame = document.getElementById("frame");
+            frame.src = "https://image.pollinations.ai/prompt/"+ prompt + "?width=2048&height=2048&nologo=true";
+        });
+    </script>
+  </body>
+</html>
