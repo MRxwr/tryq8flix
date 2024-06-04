19cb763e6ed232e94c34b1a8ae7f8a8c586268ce
MRxwr <nasserhatab@gmail.com>
2024-06-05 00:25:46 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,4 +1,83 @@
 <?php
+require("includes/config.php");
+require("includes/functions.php");
+require('templates/simple_html_dom.php');
+
+function searchShahid(){
+	
+	$curl = curl_init();
+	curl_setopt_array($curl, array(
+	  CURLOPT_URL => "https://web5.topcinema.world/recent/",
+	  CURLOPT_RETURNTRANSFER => true,
+	  CURLOPT_ENCODING => '',
+	  CURLOPT_MAXREDIRS => 10,
+	  CURLOPT_TIMEOUT => 0,
+	  CURLOPT_FOLLOWLOCATION => true,
+	  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	  CURLOPT_CUSTOMREQUEST => 'GET',
+	));
+	$html = curl_exec($curl);
+	curl_close($curl);
+	
+	//$html = file_get_contents(getWebsite());
+	// Create a DOM object
+	$dom = str_get_html($html);
+	// Check if the DOM object is valid
+	if ($dom) {
+		$data = [
+			'shows' => []
+		];
+		// Loop through each show
+		foreach ($dom->find('.Small--Box') as $show) {
+			// Extract background-image URL from style attribute
+			$style = $show->style;
+			preg_match('/\burl\s*\(\s*[\'"]?(.*?)[\'"]?\s*\)/', $style, $matches);
+			$imageUrl = isset($matches[1]) ? $matches[1] : '';
+			$title = $show->find('.title a', 0)->plaintext;
+			$url = $show->find('.title a', 0)->href;
+			$poster = $show->find('.Poster__image', 0)->data['src'];
+			$genre = $show->find('.liList li', 0)->plaintext;
+			$resolution = $show->find('.liList li', 1)->plaintext;
+			$jsonData = [
+				'title' => $title,
+				'url' => $url,
+				'poster' => $poster,
+				'genre' => $genre,
+				'resolution' => $resolution
+			];
+
+			// Add the JSON data to the array
+			$data['shows'][] = $jsonData;
+		}
+
+		// Output the JSON array
+		echo $shows = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);die();
+	} else {
+		echo 'Error: Invalid DOM object.';
+	}
+
+	$shows = ( isset($shows) && !empty($shows) ) ? json_decode($shows,true) : array() ;
+	return $shows = $shows["shows"];
+	// Clean up the DOM object
+	$dom->clear();
+	unset($dom);
+}
+
+if( isset($_POST["type"]) && !empty($_POST["type"]) ){ 
+	$user = checkLogin();
+	if ( !empty($user["id"]) ){
+		$shows = searchShahid();
+		outputData($shows); 
+	}else{
+		$msg = "Please login first.";
+		echo $msg;
+	}
+}else{
+    $msg = "something wrong happened, Please try again.";
+    echo $msg;
+}
+
+/*
 $clientId = '386563124e58e6c';
 $imageUrl = 'https://createkuwait.com/images/15-03-2316788848811678884882.png';
 
