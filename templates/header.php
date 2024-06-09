2e304eaed8e963b035f9b452e1f0e8782cd099ac
MRxwr <nasserhatab@gmail.com>
2024-06-10 01:05:59 +0300

    save

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,7 @@
 <header class="sticky-top /*bg-white border*/" style="box-shadow:0px 0px 4px 1px rgba(0, 0, 0, 0.5); border-radius: 0px 0px 10px 10px;">
     <div class="row m-0 pt-1 w-100">
 		<div class="col-1 p-1 text-center">
-            <div data-bs-toggle="modal" data-bs-target="#settings" id="settingBtn"><p class="iconFoot"><i class="bi bi-gear"></i></p></div>
+            <div data-bs-toggle="modal" data-bs-target="#settings" id="settingBtn"><p class="iconFoot" style="color:#6a6a6a"><i class="bi bi-gear"></i></p></div>
         </div>
 		
         <div class="col-10 p-1" style="text-align-last: center;">
@@ -9,27 +9,27 @@
         </div>
 
 		<div class="col-1 p-1 text-center">
-            <div id="searchBtn"><p class="iconFoot"><i class="bi bi-search-heart"></i></p></div>
+            <div id="searchBtn"><p class="iconFoot" style="color:#6a6a6a"><i class="bi bi-search-heart"></i></p></div>
         </div>
     </div>
 	<div class="row m-0 w-100">
 		<div class="col pb-2 text-center">
-			<div id="homeBtn"><p class="iconFoot"><i class="bi bi-house"></i></p>الرئيسية</div>
+			<div id="homeBtn"><p class="iconFoot" style="color:#6a6a6a"><i class="bi bi-house"></i></p>الرئيسية</div>
         </div>
         <div class="col pb-2 text-center">
-            <div id="movieBtn"><p class="iconFoot"><i class="bi bi-camera-reels"></i></p>أفلام</div>
+            <div id="movieBtn"><p class="iconFoot" style="color:#6a6a6a"><i class="bi bi-camera-reels"></i></p>أفلام</div>
         </div>
 		<div class="col pb-2 text-center">
-			<div id="tvBtn"><p class="iconFoot"><i class="bi bi-tv"></i></p>مسلسلات</div>
+			<div id="tvBtn"><p class="iconFoot" style="color:#6a6a6a"><i class="bi bi-tv"></i></p>مسلسلات</div>
         </div>
         <div class="col pb-2 text-center">
-			<div id="animeBtn"><p class="iconFoot"><i class="bi bi-emoji-dizzy"></i></p>إنمي</div>
+			<div id="animeBtn"><p class="iconFoot" style="color:#6a6a6a"><i class="bi bi-emoji-dizzy"></i></p>إنمي</div>
         </div>
         <div class="col pb-2 text-center">
-			<div id="liveBtn"><p class="iconFoot"><i class="fa fa-futbol-o"></i></p>مباشر</div>
+			<div id="liveBtn"><p class="iconFoot" style="color:#6a6a6a"><i class="fa fa-futbol-o"></i></p>مباشر</div>
         </div>
         <div class="col pb-2 text-center">
-			<div id="newsBtn"><p class="iconFoot"><i class="fa fa-newspaper-o"></i></p>اخبار</div>
+			<div id="newsBtn"><p class="iconFoot" style="color:#6a6a6a"><i class="fa fa-newspaper-o"></i></p>اخبار</div>
         </div>
     </div>
 </header>
