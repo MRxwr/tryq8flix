3a793d693af0449bf8b716259c06a3c9bf437e49
MRxwr <nasserhatab@gmail.com>
2024-07-02 01:46:30 +0300

    save

--- requests/home.php
+++ requests/home.php
@@ -38,7 +38,7 @@ function searchShahid(){
 	//var_dump($website.$collection.$category); die();
 	$curl = curl_init();
 	curl_setopt_array($curl, array(
-	  CURLOPT_URL => "https://app.scrapingbee.com/api/v1/?api_key=IN9YLTOE0MBVC5BV5GASF63BEE472R7CRTLX4N77FWZBTNZL4L3XNANQ4XMZFDN82Z6IVRQ4BAVH8GR6&stealth_proxy=true&url=". urlencode("{$website}{$collection}{$category}"),
+	  CURLOPT_URL => "https://app.scrapingbee.com/api/v1/?api_key=IN9YLTOE0MBVC5BV5GASF63BEE472R7CRTLX4N77FWZBTNZL4L3XNANQ4XMZFDN82Z6IVRQ4BAVH8GR6&url=". urlencode("{$website}{$collection}{$category}"),
 	  CURLOPT_RETURNTRANSFER => true,
 	  CURLOPT_ENCODING => '',
 	  CURLOPT_MAXREDIRS => 10,
