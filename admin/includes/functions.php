7625b5a480f4aba71d8586a89d1e876e49dc84b7
MRxwr <nasserhatab@gmail.com>
2024-07-05 02:33:13 +0300

    Update functions.php

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -230,6 +230,7 @@ function outputData($shows){
 				$shows[$i]["episode"] = "تشغيل";
 			}
 			$realTitle = explode("الحلقة",$shows[$i]["title"]);
+			@$epNumber = $realTitle[1];
 			$output .= "
 				<div class='col-xl-3 col-lg-4 col-md-4 col-sm-12 p-3'>
 					<div class='card w-100'>
@@ -239,7 +240,7 @@ function outputData($shows){
 								<h4 class='card-title {$catgoryType}' id='".str_replace(' ','-',$shows[$i]["category"])."' style='color:#9f8d5c'><b>{$shows[$i]["category"]}</b></h2>
 								<h5 class='card-title postTitle{$i}'>{$realTitle[0]}</h3>
 								<p class='card-text'>
-									<b>العنوان:</b> {$realTitle[1]}<br>
+									<b>العنوان:</b> {$epNumber}<br>
 									<b>التفاصيل:</b> {$shows[$i]["description"]}
 								</p>
 							</div>
