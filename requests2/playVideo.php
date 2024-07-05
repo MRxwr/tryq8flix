e24b54ab8010e93702a66c71558da517a9fa2458
MRxwr <nasserhatab@gmail.com>
2024-07-05 03:34:34 +0300

    Update playVideo.php

--- requests2/playVideo.php
+++ requests2/playVideo.php
@@ -1,7 +1,22 @@
 <?php
 function openVideo($id,$i,$link) {
-	$link = str_replace("web","web5",str_replace(".cam",".world",$link));
+	
 	var_dump($id,$i,$link."watch/");
+	$curl = curl_init();
+	curl_setopt_array($curl, array(
+	  CURLOPT_URL => "{$link}watch/",
+	  CURLOPT_RETURNTRANSFER => true,
+	  CURLOPT_ENCODING => '',
+	  CURLOPT_MAXREDIRS => 10,
+	  CURLOPT_TIMEOUT => 0,
+	  CURLOPT_FOLLOWLOCATION => true,
+	  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	  CURLOPT_CUSTOMREQUEST => 'GET',
+	));
+	$html = curl_exec($curl);
+	curl_close($curl);
+
+	$link = str_replace("web","web5",str_replace(".cam",".world",$link));
 	$curl = curl_init();
 	curl_setopt_array($curl, array(
 	CURLOPT_URL => 'https://web5.topcinema.world/wp-content/themes/movies2023/Ajaxat/Single/Server.php',
