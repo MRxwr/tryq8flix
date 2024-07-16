c9719a139af3f26436f92c599a80c7c3daf3848b
MRxwr <nasserhatab@gmail.com>
2024-07-17 02:48:19 +0300

    Update test.php

--- test.php
+++ test.php
@@ -8,7 +8,9 @@ function checkWithProxy($url, $proxy) {
     curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
     curl_setopt($ch, CURLOPT_PROXY, $proxy);
     curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_HTTP);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 60); // Increased timeout
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Ignore SSL certificate issues
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); // Ignore SSL certificate issues
 
     $response = curl_exec($ch);
     $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
@@ -26,14 +28,22 @@ function checkWithProxy($url, $proxy) {
     echo substr($response, 0, 500) . "...\n\n";
 }
 
-$url = "https://shvip.cam/";
-$proxies = [
-    "15.236.106.236:3128",  // The proxy that worked before
-    "162.245.85.220:80",  // Replace with actual proxies
+// Test URLs
+$urls = [
+    "https://egydead.space/home",
+    "http://egydead.space/home", // Try HTTP version
 ];
 
-foreach ($proxies as $proxy) {
-    checkWithProxy($url, $proxy);
+// Proxies to test
+$proxies = [
+    "15.236.106.236:3128",
+    "162.245.85.220:80",
+];
+
+foreach ($urls as $url) {
+    foreach ($proxies as $proxy) {
+        checkWithProxy($url, $proxy);
+    }
 }
 
 ?>
