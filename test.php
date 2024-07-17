33cd6686e28e6b3b996390b8116fefa0026b9e29
MRxwr <nasserhatab@gmail.com>
2024-07-17 03:13:53 +0300

    Update test.php

--- test.php
+++ test.php
@@ -45,7 +45,7 @@ function makeRequest($url, $proxy) {
 }
 
 $url = 'https://shvip.cam/';
-$proxy = '15.236.106.236:3128';  // Your proxy
+$proxy = 'http://45.8.21.156:80';  // Your proxy
 
 $result = makeRequest($url, $proxy);
 
