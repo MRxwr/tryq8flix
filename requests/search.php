ab3d2c7b75d1b41434f7ee862b30028f58e9faf0
MRxwr <nasserhatab@gmail.com>
2024-07-17 03:27:02 +0300

    Update search.php

--- requests/search.php
+++ requests/search.php
@@ -59,11 +59,11 @@ if( isset($_POST["type"]) && !empty($_POST["type"]) ){
     if( $_POST["type"] == "get" ){
         $user = checkLogin();
 		if( !empty($user["id"]) ){
-			$shows = searchShahid($_POST["search"]);
+			/*$shows = searchShahid($_POST["search"]);
 			echo "<div class='row m-0 w-100' id='content'>";
 			outputData($shows);
-			echo '<div class="col-md-12 loadMoreSearchBtn mb-3" style="text-align-last: center;" id="1"><div class="btn btn-secondary w-75" >تابع</div></div><div style="display:none" class="getSearch" id="'.$_POST["search"].'"></div></div>';
-			//echo "<iframe id='frame' src='".getWebsite($_POST["search"])."' style='width:100%;height:100vh;' sandbox='allow-same-origin allow-scripts' allowFullScreen></iframe>";
+			echo '<div class="col-md-12 loadMoreSearchBtn mb-3" style="text-align-last: center;" id="1"><div class="btn btn-secondary w-75" >تابع</div></div><div style="display:none" class="getSearch" id="'.$_POST["search"].'"></div></div>';*/
+			echo "<iframe id='frame' src='".getWebsite($_POST["search"])."' style='width:100%;height:100vh;' sandbox='allow-same-origin allow-scripts' allowFullScreen></iframe>";
 		}else{
 			$msg = "Please login first.";
 			echo $msg;
