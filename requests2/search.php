06f3494b9a727e8754b2f40d020615eb3f1d9861
MRxwr <nasserhatab@gmail.com>
2024-07-18 03:31:22 +0300

    Update search.php

--- requests2/search.php
+++ requests2/search.php
@@ -1,45 +1,31 @@
 <?php
 function searchShahid($search){
-	$curl = curl_init();
-	curl_setopt_array($curl, array(
-	  CURLOPT_URL => "https://web5.topcinema.world/?type=all&s=" . urlencode($search),
-	  CURLOPT_RETURNTRANSFER => true,
-	  CURLOPT_ENCODING => '',
-	  CURLOPT_MAXREDIRS => 10,
-	  CURLOPT_TIMEOUT => 0,
-	  CURLOPT_FOLLOWLOCATION => true,
-	  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-	  CURLOPT_CUSTOMREQUEST => 'GET',
-	));
-	$html = curl_exec($curl);
-	curl_close($curl);
-	
+	GLOBAL $website2;
+	$html = file_get_contents("{$website2}/?s={$search}");
 	$dom = str_get_html($html);
+	$data = [
+		'shows' => []
+	];
 	if ($dom) {
-		$data = [
-			'shows' => []
-		];
-		foreach ($dom->find('.Small--Box') as $show) {
-			$title = $show->find('.recent--block', 0)->title;
-			$url = $show->find('.recent--block', 0)->href;
-			$poster = $show->find('img', 0)->getAttribute('data-src');
-			$genre = $show->find('.liList li', 0)->plaintext;
-			@$resolution = $show->find('.liList li', 1)->plaintext;
-			@$imdbRating = $show->find('.liList li', 2)->plaintext;
+		foreach ($dom->find('.Block--Item') as $show) {
+			$link = $show->find('a', 0);
+			$image = $show->find('img', 0);
+			$genre = $show->find('.Genres li', 0);
+			$title = $show->find('h3', 0);
 			$jsonData = [
-				'href' => $url,
-				'image' => trim($poster),
-				'resolution' => $resolution,
-				'category' => $genre,
-				'title' => $title,
-				'imdbRating' => $imdbRating,
-				'description' => "",
+				'href' => $link->href,
+				'image' => $image->getAttribute('data-src'),
+				'episode' => '', // Not present in the provided HTML
+				'category' => $genre ? $genre->plaintext : '',
+				'title' => $title ? $title->plaintext : '',
+				'description' => '', // Not present in the provided HTML
 			];
 			$data['shows'][] = $jsonData;
 		}
 		$shows = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 	} else {
 		echo 'Error: Invalid DOM object.';
+		$shows = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 	}
 
 	$shows = ( isset($shows) && !empty($shows) ) ? json_decode($shows,true) : array() ;
