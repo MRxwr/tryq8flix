478a75b9f18a07a2bdfa4689b7fc48e8c86aeeca
MRxwr <nasserhatab@gmail.com>
2024-07-19 00:35:15 +0300

    save

--- js/js2.js
+++ js/js2.js
@@ -476,7 +476,7 @@ $(document).on('click', '#tvBtn', function(e) {
 	
 	$("#liveBtn").attr("style","color:#5e5e5e");
 	
-    $.post("requests2/index.php?type=home&collection=last_eps", {
+    $.post("requests2/index.php?type=home&category=مسلسلات-اجنبي/?key=episodes", {
             type: "get",
         },
         function(data) {
