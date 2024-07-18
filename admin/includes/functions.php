2f46323ebfed701052cd8ba88d8657f220dce72d
MRxwr <nasserhatab@gmail.com>
2024-07-18 23:39:27 +0300

    Update functions.php

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -283,7 +283,7 @@ function outputData2($shows){
 				<div class='col-xl-3 col-lg-4 col-md-4 col-sm-12 p-3'>
 					<div class='card w-100'>
 						<div class='card-body'>
-							<img src='".base64_encode("{$shows[$i]["image"]}")."' style='width:100%;height:300px;border-radius: 10px; box-shadow: 0px 0px 10px 0px black;'>
+							<img src='".convertImage("{$shows[$i]["image"]}")."' style='width:100%;height:300px;border-radius: 10px; box-shadow: 0px 0px 10px 0px black;'>
 							<div style='height:150px; overflow:auto;text-align: -webkit-right;' class='pt-2'>
 								<h4 class='card-title {$catgoryType}' id='".str_replace(' ','-',$shows[$i]["category"])."' style='color:#9f8d5c'><b>{$shows[$i]["category"]}</b></h2>
 								<h5 class='card-title postTitle{$i}'>{$shows[$i]["title"]}</h3>
@@ -362,7 +362,7 @@ function outputImage($imageUrl) {
 
 // make function to convert image url to base64
 function convertImage($imageUrl) {
-	$image = scrapePage($imageUrl);
+	$image = file_get_contents($imageUrl);
 	return base64_encode($image);
 }
 ?>
