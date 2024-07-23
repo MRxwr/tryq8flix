a698bf31b033aba04bd43d58e163dbd11eec1cb6
MRxwr <nasserhatab@gmail.com>
2024-07-24 01:55:37 +0300

    Update index.php

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ $profileData = checkLogin();
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
     <script src="js/main.js?y=<?php echo md5(time()) ?>"></script>
-    <script src="js/js.js?y=<?php echo md5(time()) ?>"></script>
+    <script src="js/js2.js?y=<?php echo md5(time()) ?>"></script>
 	<style>
 	body{
 		background-color: #101010;
