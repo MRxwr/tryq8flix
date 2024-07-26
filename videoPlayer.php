5f3cd195ff3d393c5160fbc31d444dc8e750d8f9
MRxwr <nasserhatab@gmail.com>
2024-07-26 04:21:37 +0300

    Update videoPlayer.php

--- videoPlayer.php
+++ videoPlayer.php
@@ -10,25 +10,34 @@ function extractVideoSource($html) {
     return null;
 }
 
+function getUrlBase($url) {
+    return strtok($url, '?');
+}
+
 if( isset($_GET["link"]) && !empty($_GET["link"]) ){
-    $curl = curl_init();
-    curl_setopt_array($curl, array(
-    CURLOPT_URL => "{$_GET["link"]}",
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => '',
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 0,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => 'GET',
-    CURLOPT_HTTPHEADER => array(
-        "referer: {$website3}",
-    ),
-    ));
-    $response = curl_exec($curl);
-    curl_close($curl);
-    $_GET["link"] = extractVideoSource($response);
-    echo "<video id='video' controls style='width:100%;'><source src='{$_GET["link"]}' type='video/mp4'></video>";
+    if ( strpos($_GET["link"], "bam") === false ) {
+        $_GET["link"] = getUrlBase($_GET["link"]);
+        echo "<iframe id='frame' src='{$_GET["link"]}' style='width:100%;height:300px' sandbox='allow-same-origin allow-scripts' allowFullScreen></iframe>";
+    }else{
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => "{$_GET["link"]}",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => array(
+            "referer: {$website3}",
+        ),
+        ));
+        $response = curl_exec($curl);
+        curl_close($curl);
+        $_GET["link"] = extractVideoSource($response);
+        echo "<video id='video' controls style='width:100%;'><source src='{$_GET["link"]}' type='video/mp4'></video>";
+    }
 }else{
     echo "لا يوجد روابط متاحه للمشاهده حاليا، الرجاء المحاولة لاحقاً";
 }
