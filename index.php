26373d7c065d8e32f9e3e46ad1d76242105f5614
MRxwr <nasserhatab@gmail.com>
2024-07-28 02:39:05 +0300

    Update index.php

--- index.php
+++ index.php
@@ -81,14 +81,14 @@ $js = (isset($_GET['js'])) ? urldecode($_GET['js']) : 'js/js3.js?y=' . md5(time(
 		?> 
     <div id="mainBody">
 		<div class="row m-0 p-o w-100">
-			<div class="col-md-4 p-3 m-3">
-				<button class="btn btn-warning rounded scrapBtn" id="<?php echo urlencode("js/js3.js") ?>">Server 1</button>
+			<div class="col-12 p-3">
+				<button class="btn btn-warning rounded scrapBtn w-100 p-5" id="<?php echo urlencode("js/js3.js") ?>">Server 1</button>
 			</div>
-			<div class="col-md-4 p-3 m-3">
-				<button class="btn btn-warning rounded scrapBtn" id="<?php echo urlencode("js/js2.js") ?>">Server 2</button>
+			<div class="col-12 p-3">
+				<button class="btn btn-warning rounded scrapBtn w-100 p-5" id="<?php echo urlencode("js/js2.js") ?>">Server 2</button>
 			</div>
-			<div class="col-md-4 p-3 m-3">
-				<button class="btn btn-warning rounded scrapBtn" id="<?php echo urlencode("js/js.js") ?>">Server 3</button>
+			<div class="col-12 p-3">
+				<button class="btn btn-warning rounded scrapBtn w-100 p-5" id="<?php echo urlencode("js/js.js") ?>">Server 3</button>
 			</div>
 		</div>
         <?php require("templates/content.php"); ?>
