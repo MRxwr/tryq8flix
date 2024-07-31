9f0c1fdbe58f32d2f4ffc3a9b57c55c3f5774cbd
MRxwr <nasserhatab@gmail.com>
2024-08-01 02:06:47 +0300

    Update getImages.php

--- requests3/getImages.php
+++ requests3/getImages.php
@@ -1,8 +1,19 @@
 <?php
 function outputImage2($imageUrl) {
-    //header('Content-Type: image/jpg');
-    $image = curlCall($imageUrl);
-    echo $image;
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => "{$imageUrl}",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'GET',
+    ));
+    $response = curl_exec($curl);
+    curl_close($curl);
+    echo $response;
 }
 outputImage2($_GET["url"]);
 ?>
