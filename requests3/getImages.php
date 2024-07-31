02dc4c3c966add7429c8ca4a3d871e342b589496
MRxwr <nasserhatab@gmail.com>
2024-08-01 02:08:42 +0300

    Update getImages.php

--- requests3/getImages.php
+++ requests3/getImages.php
@@ -9,7 +9,9 @@ function outputImage2($imageUrl) {
     CURLOPT_TIMEOUT => 0,
     CURLOPT_FOLLOWLOCATION => true,
     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    CURLOPT_HTTPHEADER => array(
+        'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+      ),
     CURLOPT_CUSTOMREQUEST => 'GET',
     ));
     $response = curl_exec($curl);
