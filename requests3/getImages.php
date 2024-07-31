6a61bd27ed74e7accb97ad592431a655bd5e7031
MRxwr <nasserhatab@gmail.com>
2024-08-01 02:03:40 +0300

    Update getImages.php

--- requests3/getImages.php
+++ requests3/getImages.php
@@ -1,6 +1,6 @@
 <?php
 function outputImage2($imageUrl) {
-    header('Content-Type: image/jpg');
+    //header('Content-Type: image/jpg');
     $image = file_get_contents($imageUrl);
     echo $image;
 }
