259dc1ab215ef6102f448f880941601ba8d63eda
MRxwr <nasserhatab@gmail.com>
2024-08-01 02:10:46 +0300

    save

--- requests3/getImages.php
+++ requests3/getImages.php
@@ -1,24 +1,3 @@
 <?php
-function outputImage2($imageUrl) {
-    GLOBAL $website2;
-    $curl = curl_init();
-    curl_setopt_array($curl, array(
-    CURLOPT_URL => "{$imageUrl}",
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => '',
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 0,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_HTTPHEADER => array(
-        'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-        "referer: {$website2}",
-      ),
-    CURLOPT_CUSTOMREQUEST => 'GET',
-    ));
-    $response = curl_exec($curl);
-    curl_close($curl);
-    echo $response;
-}
-outputImage2($_GET["url"]);
+outputImage($_GET["url"]);
 ?>
