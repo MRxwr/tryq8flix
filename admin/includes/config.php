e9abe533e04916b12fc90ef17427d5ef3053b55e
MRxwr <nasserhatab@gmail.com>
2024-09-26 01:18:14 +0300

    Update config.php

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -14,7 +14,7 @@ if (!$dbconnect) {
 }
 date_default_timezone_set('Asia/Kuwait');
 $date = date('Y-m-d H:i:s');
-$website = "https://shed4u1.net/"; 
+$website = "https://shahid4u.diy/";//"https://shed4u1.net/"; 
 $website2 = "https://web5.topcinema.world";//"https://tuktukcima.art";
 $website3 = "https://wecima.show";
 $website4 = "https://26y1dqao5h.sbs/h/"; //egydead
