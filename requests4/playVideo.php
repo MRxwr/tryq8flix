e92430e80d095560723fd80eec94ff892b4809c9
MRxwr <nasserhatab@gmail.com>
2024-09-26 02:06:11 +0300

    Update playVideo.php

--- requests4/playVideo.php
+++ requests4/playVideo.php
@@ -1,23 +1,40 @@
 <?php
 if (isset($_POST["id"]) && !empty($_POST["id"])) {
-    $html = file_get_contents("{$_POST["id"]}");
+
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => "{$_POST["id"]}",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS => array('View' => '1'),
+    ));
+
+    $html = curl_exec($curl);
+    curl_close($curl);
     $dom = str_get_html($html);
     $data = [
         'shows' => []
     ];
     if ($dom) {
-        foreach ($dom->find('.WatchServersList ul li') as $server) {
-            $btn = $server->find('btn', 0);
-            if ($btn) {
-                $title = $btn->find('strong', 0)->plaintext;
-                $dataUrl = $btn->getAttribute('data-url');
+        $serversList = $dom->find('.watchAreaMaster .serversList', 0);
+        if ($serversList) {
+            foreach ($serversList->find('li') as $server) {
+                $link = $server->getAttribute('data-link');
                 $jsonData = [
-                    'link' => str_replace(" ", "", $dataUrl)
+                    'link' => str_replace(" ", "", $link)
                 ];
                 $data['shows'][] = $jsonData;
             }
+            $servers = json_encode($data['shows'], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+        } else {
+            echo 'Error: Server list not found.';
+            $servers = json_encode([], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
         }
-        $servers = json_encode($data['shows'], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
     } else {
         echo 'Error: Invalid DOM object.';
         $servers = json_encode([], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
@@ -30,7 +47,7 @@ if (isset($_POST["id"]) && !empty($_POST["id"])) {
     for ($i = 0; $i < sizeof($servers); $i++) {
             $url = $servers[$i]["link"];
             $serverDetails = json_encode($servers[$i]);
-            $links .= "<div class='col-3 p-1'><a class='btn btn-secondary w-100' style='color:white' href='#' onclick='sendIdToIframe2(\"{$url}\"); return false;'>Serv-{$y}</a></div>";
+            $links .= "<div class='col-3 p-1'><a class='btn btn-secondary w-100' style='color:white' href='#' onclick='sendIdToIframe(\"{$url}\"); return false;'>Serv-{$y}</a></div>";
             $mainServer[] = $url;
             $y++;
             if( $i == 3 ){
