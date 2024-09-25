8393f9d59f9a9ec82a2ee86cc6f63c5ca1033898
MRxwr <nasserhatab@gmail.com>
2024-09-26 02:28:46 +0300

    Update more.php

--- requests4/more.php
+++ requests4/more.php
@@ -1,12 +1,23 @@
 <?php
+function extractSeasonUrl($html) {
+    if (preg_match('/<a itemprop="url" href="(https:\/\/[^"]*\/season\/[^"]*)"/', $html, $matches)) {
+        return $matches[1];
+    }
+    return null;
+}
+
 if( isset($_POST["id"]) && !empty($_POST["id"]) ){
-    $html = file_get_contents($_POST["id"]);
+    $html = extractSeasonUrl($_POST["id"]);
+    $html = scrapEgyDead($html);
     $htmlDom = str_get_html($html);
     $seasonsData = [];
     $episodesData = [];
-    foreach ($htmlDom->find('.List--Seasons--Episodes a') as $seasonLink) {
+
+    // Scrape seasons
+    foreach ($htmlDom->find('.seasons-list .movieItem') as $seasonItem) {
+        $seasonLink = $seasonItem->find('a', 0);
         $link = $seasonLink->href;
-        $title = trim($seasonLink->plaintext);
+        $title = $seasonLink->title;
         $seasonNumber = preg_replace('/[^0-9]/', '', $title);
         $seasonsData[] = [
             'link' => $link,
@@ -14,12 +25,13 @@ if( isset($_POST["id"]) && !empty($_POST["id"]) ){
             'season_number' => $seasonNumber
         ];
     }
-    // Scrape episodes
-    foreach ($htmlDom->find('.Episodes--Seasons--Episodes a') as $episodeLink) {
-        $link = $episodeLink->href;
-        $title = trim($episodeLink->find('episodetitle', 0)->plaintext);
-        $episodeNumber = preg_replace('/[^0-9]/', '', $title);
 
+    // Scrape episodes
+    foreach ($htmlDom->find('.episodes-list .EpsList li') as $episodeItem) {
+        $episodeLink = $episodeItem->find('a', 0);
+        $link = $episodeLink->href;
+        $title = $episodeLink->title;
+        $episodeNumber = preg_replace('/[^0-9]/', '', $episodeLink->plaintext);
         $episodesData[] = [
             'link' => $link,
             'title' => $title,
@@ -27,20 +39,22 @@ if( isset($_POST["id"]) && !empty($_POST["id"]) ){
         ];
     }
 
-	if (strpos(strtolower($_POST["id"]), 'season') === false){
-		$episodesData = array_reverse($episodesData);
-		$seasonsData = array_reverse($seasonsData);
-	}
+    // Reverse arrays if not a season page
+    if (strpos(strtolower($_POST["id"] ?? ''), 'season') === false) {
+        $episodesData = array_reverse($episodesData);
+        $seasonsData = array_reverse($seasonsData);
+    }
+
 	
     if (strpos(strtolower($_POST["id"]), 'season') === false){
 		for( $i = 0; $i < sizeof($seasonsData); $i++){
-			echo "<div data-bs-toggle='modal' data-bs-target='#threeDots2'  class='btn btn-warning m-1 threeDots2' id='".str_replace("film","watch",str_replace("post","watch",str_replace("episode","watch",$seasonsData[$i]["link"])))."'>{$seasonsData[$i]["title"]}</div>";
+			echo "<div data-bs-toggle='modal' data-bs-target='#threeDots2'  class='btn btn-warning m-1 threeDots2' id='".$seasonsData[$i]["link"]."'>{$seasonsData[$i]["title"]}</div>";
 		}
 		echo "<hr>";
 	}
 	
 	for( $i = 0; $i < sizeof($episodesData); $i++){
-		echo "<div data-bs-toggle='modal' data-bs-target='#playVideo'  class='btn btn-danger m-1 playVideo' id='".str_replace("film","watch",str_replace("post","watch",str_replace("episode","watch",$episodesData[$i]["link"])))."'>{$episodesData[$i]["title"]}</div>";
+		echo "<div data-bs-toggle='modal' data-bs-target='#playVideo'  class='btn btn-danger m-1 playVideo' id='".$episodesData[$i]["link"]."'>{$episodesData[$i]["title"]}</div>";
 	}
 	
 	if( sizeof($episodesData) < 1 &&  sizeof($seasonsData) < 1 ){
