567a1ee3a4d08613d11248793b6267ae9ad02977
MRxwr <nasserhatab@gmail.com>
2024-09-26 02:19:16 +0300

    Update functions.php

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -308,6 +308,51 @@ function outputData2($shows){
 	}
 }
 
+function outputData3($shows){ 
+	$user = checkLogin();
+	$output = "";
+	if( is_array($shows) && !empty($shows) && !empty($user["id"]) ){
+		for ($i = 0; $i < sizeof($shows); $i++) {
+			$checkVideoType = $shows[$i]["href"];
+			if( strstr($shows[$i]["href"],"episode") ){
+				$catgoryType = "categoryTitleTv";
+				$shows[$i]["episode"] = $shows[$i]["episode"];
+			}elseif( strstr($shows[$i]["href"],"film") ){
+				$catgoryType = "categoryTitleMovie";
+				$shows[$i]["episode"] = "تشغيل";
+			}else{
+				$catgoryType = "categoryTitlePost";
+				$shows[$i]["episode"] = "تشغيل";
+			}
+			$output .= "
+				<div class='col-xl-3 col-lg-4 col-md-6 col-sm-12 p-3'>
+					<div class='card w-100'>
+						<div class='card-body'>
+							<img src='{$shows[$i]["image"]}' style='width:100%;height:300px;border-radius: 10px; box-shadow: 0px 0px 10px 0px black;'>
+							<div style='height:150px; overflow:auto;text-align: -webkit-right;' class='pt-2'>
+								<h4 class='card-title {$catgoryType}' id='".str_replace(' ','-',$shows[$i]["category"])."' style='color:#9f8d5c'><b>{$shows[$i]["category"]}</b></h2>
+								<h5 class='card-title postTitle{$i}'>{$shows[$i]["title"]}</h3>
+							</div>
+							<div class='row w-100 p-0 m-0'>
+								<div class='col-6 p-1'>
+									<div data-bs-toggle='modal' data-bs-target='#playVideo' class='btn btn-danger w-100 playVideo' id='{$checkVideoType}'><i class='bi bi-play-fill'></i> {$shows[$i]["episode"]}</div>
+								</div>
+								<div class='col-6 p-1'>
+									<div data-bs-toggle='modal' data-bs-target='#threeDots' class='btn btn-warning w-100 threeDots' id='{$shows[$i]["href"]}'><i class='bi bi-three-dots'></i></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			";
+		}
+		echo $output;
+	}else{
+		$msg = "<h1 class='text-center mt-5'>No result.<h1>";
+		echo $msg;
+	}
+}
+
 function scrapePage($url) {
 	GLOBAL $scrappingBeeToken;
 /*
