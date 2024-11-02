649180691cedbb1f51edb680def80cde6b9df370
MRxwr <nasserhatab@gmail.com>
2024-11-03 00:21:42 +0300

    Update functions.php

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -452,7 +452,8 @@ function domTopCinema($dom) {
 }
 
 function scrapEgyDead($url) {
-	$html = curlCall($url);
+	$html = makeRequest2($url);
+	var_dump($html);
 	$dom = str_get_html($html);
 	$mainSection = $dom->find('.main-section', 0);
 	if (strpos($url, 'category') !== false) {
@@ -528,6 +529,39 @@ function makeRequest($url, $postData = null, $referer = null) {
     curl_close($ch);
     $link = extractLink($response);
     return $link;
+}
+
+function makeRequest2($url, $postData = null, $referer = null) {
+    $ch = curl_init();
+    $headers = [
+        'Accept: */*',
+        'Accept-Language: en-US,en;q=0.5',
+        'Accept-Encoding: gzip, deflate',
+        'X-Requested-With: XMLHttpRequest',
+        'Connection: keep-alive',
+        'Sec-Fetch-Dest: empty',
+        'Sec-Fetch-Mode: cors',
+        'Sec-Fetch-Site: same-origin',
+    ];
+    if ($referer) {
+        $headers[] = 'Referer: ' . $referer;
+    }
+    curl_setopt_array($ch, [
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HEADER => false,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0',
+        CURLOPT_HTTPHEADER => $headers,
+        CURLOPT_ENCODING => '',
+    ]);
+    if ($postData) {
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postData));
+    }
+    $response = curl_exec($ch);
+    curl_close($ch);
+    return $response;
 }
 
 function extractLink($html) {
