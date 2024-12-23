0eae3d9da77ae64d551a3479eb5b8af92b2f4134
MRxwr <nasserhatab@gmail.com>
2024-12-24 00:58:36 +0300

    save

--- js/js.js
+++ js/js.js
@@ -101,12 +101,11 @@ $(document).on('click', '#loginBtn', function(e) {
 
 function bodyLoad() {
     $("#loading-screen").show();
-    if ($.cookie('tryq8flix2') != null) {
+    if ($.cookie('tryq8flix2')) {
         $("#profileOptions0").attr("style", "display:none");
         $("#profileOptions1").attr("style", "display:block");
 		$("#homeBtn").attr("style","color:#9f8d5c");
 		$(".bi-house").addClass("bi-house-fill").removeClass("bi-house");
-        $("#loading-screen").hide();
 		$.post("requests/index.php?type=home", {
 			type: "get",
 		},
