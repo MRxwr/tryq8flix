10433a991c3ccd575a8a5f6a89652b77592d9e57
MRxwr <nasserhatab@gmail.com>
2024-12-24 01:02:39 +0300

    Update js.js

--- js/js.js
+++ js/js.js
@@ -119,7 +119,7 @@ function bodyLoad() {
     }
 }
 
-$(document).on('load', function(e) {
+$(document).on('ready', function(e) {
     alert($.cookie('tryq8flix2'));
     if ($.cookie('tryq8flix2') != null) {
         $("#profileOptions0").attr("style", "display:none");
