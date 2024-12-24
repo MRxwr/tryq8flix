437d0c5c432277cd5ab20bf5db3f0108230e4912
MRxwr <nasserhatab@gmail.com>
2024-12-25 00:05:48 +0300

    Update index.php

--- index.php
+++ index.php
@@ -112,6 +112,20 @@ $xValue = md5(time());
 		window.location.href = "?js=" + btnId;
 		$("#loading-screen").show();
 	});
+	// i want to move from div to div that has nextBtn as class using keyboard arrow keys
+	$(document).on('keydown', function(e) {
+		if (e.keyCode == 37) {
+			var nextBtn = $('.nextBtn');
+			if (nextBtn.length > 0) {
+				nextBtn[0].click();
+			}
+		} else if (e.keyCode == 39) {
+			var prevBtn = $('.prevBtn');
+			if (prevBtn.length > 0) {
+				prevBtn[0].click();
+			}
+		}
+	});
 </script>
 </body>
 
