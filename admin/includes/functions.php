369696e8a0ec7bbe6b9a9ad25f078ae1a3f10a93
MRxwr <nasserhatab@gmail.com>
2024-12-29 00:10:03 +0300

    Update functions.php

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -244,7 +244,7 @@ function outputData($shows){
 									<img src='requests?type=getImages&url={$shows[$i]["image"]}' style='width:100%;height:150px;border-radius: 10px; box-shadow: 0px 0px 10px 0px black;'>
 								</div>
 								<div class='col-8 p-1'>
-									<div style='height:250px; overflow:auto;text-align: -webkit-right;' class='pt-2'>
+									<div style='height:170px; overflow:auto;text-align: -webkit-right;' class='pt-2'>
 										<h6 class='card-title {$catgoryType}' id='".str_replace(' ','-',$shows[$i]["category"])."' style='color:#9f8d5c'><b>{$shows[$i]["category"]}</b></h6>
 										<h5 class='card-title postTitle{$i}'>{$realTitle[0]}</h5>
 										<p class='card-text'>
