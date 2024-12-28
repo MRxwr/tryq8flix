1434619d1c91ef16b8042d6d10fe727b22b698c7
MRxwr <nasserhatab@gmail.com>
2024-12-29 00:03:29 +0300

    Update functions.php

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -239,7 +239,11 @@ function outputData($shows){
 				<div class='col-xl-2 col-lg-4 col-md-6 col-sm-12 p-3'>
 					<div class='card w-100'>
 						<div class='card-body'>
+							<div class='row w-100 p-0 m-0'>
+								<div class='col-4 p-1'>
 							<img src='requests?type=getImages&url={$shows[$i]["image"]}' style='width:100%;height:150px;border-radius: 10px; box-shadow: 0px 0px 10px 0px black;'>
+							</div>
+							<div class='col-8 p-1'>
 							<div style='height:250px; overflow:auto;text-align: -webkit-right;' class='pt-2'>
 								<h4 class='card-title {$catgoryType}' id='".str_replace(' ','-',$shows[$i]["category"])."' style='color:#9f8d5c'><b>{$shows[$i]["category"]}</b></h4>
 								<h5 class='card-title postTitle{$i}'>{$realTitle[0]}</h5>
@@ -247,6 +251,8 @@ function outputData($shows){
 									<b>العنوان:</b> {$shows[$i]["episode"]}<br>
 									<b>التفاصيل:</b> {$shows[$i]["description"]}
 								</p>
+							</div>
+							</div>
 							</div>
 							<div class='row w-100 p-0 m-0'>
 								<div class='col-6 p-1'>
