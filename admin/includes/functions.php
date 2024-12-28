149d6a87bd0c3d631530a778457682b979d9a96b
MRxwr <nasserhatab@gmail.com>
2024-12-29 00:01:21 +0300

    Update functions.php

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -241,11 +241,11 @@ function outputData($shows){
 						<div class='card-body'>
 							<img src='requests?type=getImages&url={$shows[$i]["image"]}' style='width:100%;height:150px;border-radius: 10px; box-shadow: 0px 0px 10px 0px black;'>
 							<div style='height:250px; overflow:auto;text-align: -webkit-right;' class='pt-2'>
-								<h4 class='card-title {$catgoryType}' id='".str_replace(' ','-',$shows[$i]["category"])."' style='color:#9f8d5c'><b>{$shows[$i]["category"]}</b></h2>
-								<h5 class='card-title postTitle{$i}'>{$realTitle[0]}</h3>
+								<h4 class='card-title {$catgoryType}' id='".str_replace(' ','-',$shows[$i]["category"])."' style='color:#9f8d5c'><b>{$shows[$i]["category"]}</b></h4>
+								<h5 class='card-title postTitle{$i}'>{$realTitle[0]}</h5>
 								<p class='card-text'>
 									<b>العنوان:</b> {$shows[$i]["episode"]}<br>
-									<b>التفاصيل:</b> {".substr($shows[$i]["description"],0,75)."}...
+									<b>التفاصيل:</b> {$shows[$i]["description"]}
 								</p>
 							</div>
 							<div class='row w-100 p-0 m-0'>
