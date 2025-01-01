d4d73f340f5ea849e8c5c3f8661afd124aa6eb22
MRxwr <nasserhatab@gmail.com>
2025-01-01 04:13:33 +0300

    Update modals.php

--- templates/modals.php
+++ templates/modals.php
@@ -1,5 +1,5 @@
 <div class="modal fade" id="threeDots" tabindex="-1" aria-labelledby="" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
+    <div class="modal-dialog modal-fullscreen">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="moreTitle"></h5>
@@ -38,7 +38,7 @@
 </div>
 
 <div class="modal fade" id="settings" tabindex="-1" aria-labelledby="" aria-hidden="true">
-    <div class="modal-dialog modal-fullscreen">
+    <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="">Settings</h5>
