e13dcc322ce4ca29738273f501970a8b3db7d599
MRxwr <nasserhatab@gmail.com>
2025-01-31 03:37:53 +0300

    Update search.php

--- requests4/search.php
+++ requests4/search.php
@@ -2,7 +2,7 @@
 function searchShahid($search){
 	GLOBAL $website4;
 	$search = str_replace(' ','+',$search);
-	$html = scrapEgyDead("{$website4}/?s={$search}");
+	$html = scrapEgyDead("https://9zb63om0bp.sbs/?s={$search}");
 	return $html;
 }
 
