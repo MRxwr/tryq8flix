6ed705a22edf52c3a53e8a9c5813f4c668156411
MRxwr <nasserhatab@gmail.com>
2025-02-07 01:07:39 +0300

    Update apiHome.php

--- api/views/apiHome.php
+++ api/views/apiHome.php
@@ -9,7 +9,11 @@ if( isset($_GET["action"]) && !empty($_GET["action"]) ){
     }elseif( $_GET["action"] == "view" ){
         if( isset($_GET["server"]) && !empty($_GET["server"]) ){
             if( $_GET["server"] == 1 ){
-                $data = scrapeWecima("{$website3}");
+                $url = $website3;
+                if( isset($_GET["page"]) && !empty($_GET["page"]) ){
+                    $url .= "/page/{$_GET["page"]}";
+                }
+                $data = scrapeWecima("{$url}");
                 $data = json_decode($data, true);
                 echo dataOutput($data);die();
             }else{
