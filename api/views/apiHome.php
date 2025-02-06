b0a5ceea180132aa72ff371f953a5ea5570c4a3d
MRxwr <nasserhatab@gmail.com>
2025-02-07 00:47:26 +0300

    save

---
+++ api/views/apiHome.php
@@ -0,0 +1,27 @@
+<?php
+if( isset($_GET["action"]) && !empty($_GET["action"]) ){
+    if( empty($token) ){
+        echo dataError(array("msg" => "token is required"));die();
+    }
+    if( $_GET["action"] == "list" ){
+        $data = array();
+        echo dataOutput($data);die();
+    }elseif( $_GET["action"] == "view" ){
+        if( isset($_GET["server"]) && !empty($_GET["server"]) ){
+            if( $_GET["server"] == 1 ){
+                $data = scrapeWecima($url);
+                $data = json_decode($data, true);
+                echo dataOutput($data);die();
+            }else{
+                echo dataError(array("msg" => "Invalid Server"));die();
+            }
+        }else{
+            echo dataError(array("msg" => "server is required"));die();
+        }
+    }else{
+        echo dataError(array("msg" => "404 action Not Found"));die();
+    }
+}else{
+    echo dataError(array("msg" => "404 action Not Found"));die();
+}
+?>
