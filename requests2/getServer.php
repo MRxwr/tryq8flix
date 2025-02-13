70e4def8bcb2645e7c6bbee272c8560d49d5e53e
MRxwr <nasserhatab@gmail.com>
2025-02-14 01:13:38 +0300

    Update getServer.php

--- requests2/getServer.php
+++ requests2/getServer.php
@@ -3,7 +3,7 @@
 if( isset($_POST["type"]) && !empty($_POST["type"]) && $_POST["type"] == "get" ){
     echo $ajaxUrl = "{$website2}/wp-content/themes/movies2023/Ajaxat/Single/Server.php";
     $data = json_decode($_POST["data"], true);
-    $link = $data["link"];
+    $link = str_replace("web2.topcinema.cam","web5.topcinema.world",$data["link"]);
     unset($data["link"]);
     $postData = $data;
     echo $url = makeRequest($ajaxUrl, $postData, $link);
