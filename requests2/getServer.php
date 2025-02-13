8f9fc0a9dad6e73ebf663ee737bd41b2eb8bbf35
MRxwr <nasserhatab@gmail.com>
2025-02-14 01:52:26 +0300

    Update getServer.php

--- requests2/getServer.php
+++ requests2/getServer.php
@@ -25,6 +25,7 @@ function getTopCimaUrl($postData, $link){
         'content-type: application/x-www-form-urlencoded',
         'content-length: 13',
         'user-agent: Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1 Edg/133.0.0.0',
+        'origin: https://web5.topcinema.world'
     ),
     ));
     $response = curl_exec($curl);
