ec4b39994beb5d48d76a621fe010fbb7ff8fd356
MRxwr <nasserhatab@gmail.com>
2025-03-03 03:18:07 +0300

    Update apiVersion.php

--- api/views/apiVersion.php
+++ api/views/apiVersion.php
@@ -1,7 +1,7 @@
 <?php
 if( isset($_GET["action"]) && $_GET["action"] == "version" ){
     $response = array(
-        "version" => "0.2.3",
+        "version" => "0.2.2",
         "iosLink" => "https://itunes.apple.com/us/app/your-app-name/id1234567890?ls=1&mt=8",
         "androidLink" => "https://play.google.com/store/apps/details?id=com.your.app.name"
     );
