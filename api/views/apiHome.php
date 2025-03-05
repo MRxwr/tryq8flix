1278076f38950abd7a76833dde8f051910415bbd
MRxwr <nasserhatab@gmail.com>
2025-03-06 02:36:33 +0300

    Update apiHome.php

--- api/views/apiHome.php
+++ api/views/apiHome.php
@@ -48,13 +48,14 @@ if( isset($_GET["action"]) && !empty($_GET["action"]) ){
                 echo dataOutput(array("shows" =>$data));die();
             }elseif( $_GET["server"] == 3 ){
                 $url = $website2 . "/recenT";
+                $_GET["page"] = ( isset($_GET["page"]) && !empty($_GET["page"]) ) ? (int)$_GET["page"] + 1 : 1 ;
                 if( isset($_GET["search"]) && !empty($_GET["search"]) ){
                     $_GET["search"] = str_replace(" ","+",$_GET["search"]);
                     $url .= "/?s={$_GET["search"]}";
                 }
-                if( isset($_GET["page"]) && !empty($_GET["page"]) && (!isset($_GET["search"]) || empty($_GET["search"])) ){
+                if( (!isset($_GET["search"]) || empty($_GET["search"])) ){
                     $url .= "/page/{$_GET["page"]}";
-                }elseif( isset($_GET["page"]) && !empty($_GET["page"]) && (isset($_GET["search"]) && !empty($_GET["search"])) ){
+                }elseif( (isset($_GET["search"]) && !empty($_GET["search"])) ){
                     $url .= "/page/{$_GET["page"]}/?s={$_GET["search"]}";
                 }
                 $data = domTopCinema("{$url}");
