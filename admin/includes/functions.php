19ba2e635bca46154c31da2e107c0269c1e08fc3
MRxwr <nasserhatab@gmail.com>
2025-03-06 03:06:30 +0300

    Update functions.php

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -587,7 +587,7 @@ function topCinemaServers($url) {
         $url1 = makeRequest($ajaxUrl, $servers[$i], "{$url}watch/");
         $mainServer["shows"][]["link"] = $url1;
     }*/
-    $url1 = makeRequest($ajaxUrl, $servers[1], "{$servers[1]["link"]}");
+    $url1 = makeRequest($ajaxUrl, $servers[1], "https://web5.topcinema.world");
     $mainServer["shows"][]["link"] = $url1;
     return $mainServer;
 }
