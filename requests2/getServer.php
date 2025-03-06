170271d593d83f2bb11f3e2e26ff8c5567870fda
MRxwr <nasserhatab@gmail.com>
2025-03-06 13:23:19 +0300

    Update getServer.php

--- requests2/getServer.php
+++ requests2/getServer.php
@@ -2,7 +2,7 @@
 if( isset($_GET["type"]) && !empty($_GET["type"]) && $_GET["type"] == "getServer" ){
     $ajaxUrl = 'https://web2.topcinema.cam/wp-content/themes/movies2023/Ajaxat/Single/Server.php';
     $data = json_decode($_POST["data"], true);
-    print_R($data);
+    print_R($_POST);
     $link = $data["link"];
     unset($data["link"]);
     $postData = $data;
