655f06e8114f803794a5f24cd94aadc59695ee55
MRxwr <nasserhatab@gmail.com>
2025-03-06 12:19:26 +0300

    Update apiVersion.php

--- api/views/apiVersion.php
+++ api/views/apiVersion.php
@@ -1,8 +1,8 @@
 <?php
 if( isset($_GET["action"]) && $_GET["action"] == "version" ){
     $response = array(
-        "version" => "1.0.1",
-        "iosLink" => "https://itunes.apple.com/us/app/your-app-name/id1234567890?ls=1&mt=8",
+        "version" => "1.0.2",
+        "iosLink" => "itms-beta://",
         "androidLink" => "https://tryq8flix.com/app-release.apk"
     );
     echo dataOutput($response);
