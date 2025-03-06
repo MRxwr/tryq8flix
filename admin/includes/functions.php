596b2545ac68054e8211d950cac2b3d9d0c44e05
MRxwr <nasserhatab@gmail.com>
2025-03-06 03:15:01 +0300

    Update functions.php

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -604,7 +604,8 @@ function topCinemaServers($url) {
         'Connection: keep-alive',
         'Sec-Fetch-Dest: empty',
         'Sec-Fetch-Mode: cors',
-        'Sec-Fetch-Site: same-origin'
+        'Sec-Fetch-Site: same-origin',
+        'cookie: _gid=GA1.2.260252561.1741219093; _gat_gtag_UA_269945118_1=1; _gat_gtag_UA_269945118_2=1; prefetchAd_6969551=true; _ga=GA1.2.1313764862.1741219093; _ga_6ZDPCTTMZN=GS1.1.1741219092.1.1.1741219141.0.0.0'
     ),
     CURLOPT_USERAGENT => 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Mobile Safari/537.36'
     ));
