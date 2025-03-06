21ad0ca2d6dc35d5f8958873c99c3cacc14d76af
MRxwr <nasserhatab@gmail.com>
2025-03-06 23:34:58 +0300

    Update videoPlayer.php

--- videoPlayer.php
+++ videoPlayer.php
@@ -5,7 +5,7 @@
     <title>Adaptive Video Player</title>
     <script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>
 </head>
-<body style="background-color: #1A1A1A;">
+<body style="background-color: #1A1A1A;margin: auto;">
     <?php 
     if (isset($_GET["server"]) && $_GET["server"] != 1 ){
         echo "<iframe id='frame' src='{$_GET["link"]}' style='width:100%;height:100vh;border: none;overflow: hidden;'allowFullScreen></iframe>"; 
