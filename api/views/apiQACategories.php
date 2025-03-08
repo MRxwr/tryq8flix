b3953ca7b238bf2c52eee9eaa534eec6a4aef06c
MRxwr <nasserhatab@gmail.com>
2025-03-09 02:04:03 +0300

    save

--- api/views/apiQACategories.php
+++ api/views/apiQACategories.php
@@ -1,11 +1,7 @@
 <?php
-
-$categories = selectDB("qas_categories","`status` = '0' AND `hidden` = '0'");
-
-if ($categories) {
+if ($categories = selectDB("qas_categories","`status` = '0' AND `hidden` = '0'")) {
     $mainCategories = [];
     $subCategories = [];
-
     foreach ($categories as $category) {
         if ($category['parentId'] == 0) {
             $mainCategories[] = [
@@ -26,14 +22,11 @@ if ($categories) {
             ];
         }
     }
-
-    // Add subcategories to their parent categories
     foreach ($mainCategories as &$mainCategory) {
         if (isset($subCategories[$mainCategory['id']])) {
             $mainCategory['subcategories'] = $subCategories[$mainCategory['id']];
         }
     }
-
     echo dataOutput($mainCategories);
 } else {
     echo dataError(array(['error' => 'No categories found']));
