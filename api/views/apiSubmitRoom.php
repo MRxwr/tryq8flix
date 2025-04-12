c4d826d1455a4994f1c31faa4d1fe985aa2bc5d5
MRxwr <nasserhatab@gmail.com>
2025-04-13 00:42:43 +0300

    Create apiSubmitRoom.php

---
+++ api/views/apiSubmitRoom.php
@@ -0,0 +1,24 @@
+<?php
+if( isset($_GET["action"]) && $_GET["action"] == "submitRoom" ){
+    if( !isset($_POST["code"]) || empty($_POST["code"]) ){
+        echo dataError(array("msg" => "Room code is required"));die();
+    }
+    if( !isset($_POST["type"]) || empty($_POST["type"]) ){
+        echo dataError(array("msg" => "Room type is required"));die();
+    }
+    if( !isset($_POST["roomData"]) || empty($_POST["roomData"]) ){
+        echo dataError(array("msg" => "Room data is required"));die();
+    }
+    if( $user = selectDB("users","`keepalive` = '{$token}'") ){
+        $user = $user[0];
+        if( $room = insertDB("qas_rooms",$_POST) ){
+            echo dataOutput(array("msg" => "Room saved successfully"));die();
+        }else{
+            echo dataError(array("msg" => "Could not save room"));die();
+        }
+    }else{
+        echo dataError(array("msg" => "Invalid token"));die();
+    }
+
+}
+?>
