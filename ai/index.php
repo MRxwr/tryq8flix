971f77710558211eacab68a8279e3332c41e7afa
MRxwr <nasserhatab@gmail.com>
2025-04-15 01:40:14 +0300

    Update index.php

--- ai/index.php
+++ ai/index.php
@@ -63,12 +63,6 @@
             
             <div class="col-12 pt-3" id="returnImage">
                 <img id="frame" src="" alt="Your generated image will appear here" width="100%" height="350" style="display: none;">
-                <script>
-                  const frame = document.getElementById('frame');
-                  frame.onload = () => {
-                    frame.style.display = 'block';
-                  };
-                </script>
             </div>
             
             <div class="col-12 pt-4">
@@ -128,6 +122,12 @@
           });
         });
       }
+    </script>
+    <script>
+        const frame = document.getElementById('frame');
+        frame.onload = () => {
+        frame.style.display = 'block';
+        };
     </script>
     <script>
       let deferredPrompt;
