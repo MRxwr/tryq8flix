2074aa93b3592802db317e3669408d455055f2ba
MRxwr <nasserhatab@gmail.com>
2025-04-15 01:35:56 +0300

    save

--- ai/index.php
+++ ai/index.php
@@ -8,7 +8,7 @@
     <meta name="author" content="TRYQ8FLiX">
     <meta http-equiv="Cache-Control" content="max-age=31536000, must-revalidate">
     <meta http-equiv="Expires" content="Tue, 15 Apr 2026 00:00:00 GMT">
-    <title>AI Art Generator - Create Stunning Art with AI</title>
+    <title>Q8 AI Art Generator - Create Stunning Art with AI</title>
     <link rel="icon" href="images/mobileLogo.png" type="image/png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
