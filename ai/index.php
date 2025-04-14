f098629e75deb0947053a75957b1ed4b161e545b
MRxwr <nasserhatab@gmail.com>
2025-04-15 00:42:49 +0300

    save

--- ai/index.php
+++ ai/index.php
@@ -100,7 +100,7 @@
         <div class="spinner"></div>
         <p>Creating Your Masterpiece... Please Wait</p>
     </div>
-    <script src="js/script.js"></script>
+    <script src="js/script.obfuscated.js" type="text/javascript" integrity="sha384-..." crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
   </body>
