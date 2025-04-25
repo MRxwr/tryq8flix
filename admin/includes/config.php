17fba880e874836f66706d5a087fd7ff5bee7e67
MRxwr <nasserhatab@gmail.com>
2025-04-26 02:54:17 +0300

    Update config.php

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -16,7 +16,7 @@ date_default_timezone_set('Asia/Kuwait');
 $date = date('Y-m-d H:i:s');
 $website = "https://sheid4u.net/";//"https://sheid4u.net/";//"https://shaa4u.cam/";
 $website2 = "https://web5.topcinema.world";//"https://tuktukcima.art";
-$website3 = "https://wecima.show";
+$website3 = "https://wecima.watch/";
 $website4 = "https://ze0shqhjbe.sbs/";
 $websiteLive = "https://shootz.yalla-shoot-tv.live/home18/"; //"https://www.yalla-kooora.live/";
 $scrappingBeeToken = "0K5RT5UBE82PSAHTQGPJW0XSFE4AR92XXK9YCXF9H0VHSUZT0P2XJQCDOO2N60S332YVUSRD5T2NDOM3";
