07c04eb94bd5b672e5ba1686c97cb6dc188bcfd3
MRxwr <nasserhatab@gmail.com>
2025-04-26 03:22:15 +0300

    save

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -707,12 +707,12 @@ function topCinemaServers($url) {
     $servers = json_decode($servers, true);
     $mainServer = [];
     $ajaxUrl = "https://tryq8flix.com/requests2/index?type=getServer";
-    $url1 = makeRequest($ajaxUrl, array("data"=>json_encode($servers[1])), "");
     $blackList = [0,3,4,5,6];
     for ($i = 0; $i < sizeof($servers); $i++) {
         if (in_array($i, $blackList)) {
         }else{
-            $mainServer[]["link"] = $servers[$i]["link"];
+            $url1 = makeRequest($ajaxUrl, array("data"=>$servers[$i]), "");
+            $mainServer[]["link"] = $url1;
         }
     }
     return $mainServer;
