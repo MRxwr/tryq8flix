9135dea2b27227956c1931c2e3dca23d1c44ec01
MRxwr <nasserhatab@gmail.com>
2025-04-26 03:23:18 +0300

    Update getServer.php

--- requests2/getServer.php
+++ requests2/getServer.php
@@ -3,6 +3,7 @@ if( isset($_GET["type"]) && !empty($_GET["type"]) && $_GET["type"] == "getServer
     $ajaxUrl = "{$website3}/wp-content/themes/movies2023/Ajaxat/Single/Server.php";
     $data = json_decode($_POST["data"], true);
     $link = $data["link"];
+    var_dump($_POST);die();
     unset($data["link"]);
     $postData = $data;
     echo $url = makeRequest($ajaxUrl, $postData, $link);
