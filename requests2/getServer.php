048af823ec9f5696389d2349ae4a7260f88a80fc
MRxwr <nasserhatab@gmail.com>
2025-04-26 04:18:20 +0300

    Update getServer.php

--- requests2/getServer.php
+++ requests2/getServer.php
@@ -5,7 +5,6 @@ if( isset($_GET["type"]) && !empty($_GET["type"]) && $_GET["type"] == "getServer
     $link = $data["link"];
     unset($data["link"]);
     $postData = $data;
-    var_dump($postData);
     echo $url = makeRequest($ajaxUrl, $postData, $link);
 }else{
     echo "";
