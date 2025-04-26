784566dbd7c2bcb976c1b3e178d4a31fbc2dc769
MRxwr <nasserhatab@gmail.com>
2025-04-26 04:00:26 +0300

    Update functions.php

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -797,7 +797,7 @@ function makeRequest($url, $postData = null, $referer = null) {
     }
     $response = curl_exec($ch);
     curl_close($ch);
-    print_r($response); die();
+    print_r($postData); die();
     $link = extractLink($response);
     return $link;
 }
