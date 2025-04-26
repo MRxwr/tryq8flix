d7dfbda9550533f61f2ab7919e797f62959d0898
MRxwr <nasserhatab@gmail.com>
2025-04-26 03:38:58 +0300

    save

--- requests2/playVideo.php
+++ requests2/playVideo.php
@@ -27,7 +27,6 @@ if (isset($_POST["id"]) && !empty($_POST["id"])) {
     $y = 1;
     $mainServer = [];
     $ajaxUrl = "{$website2}/wp-content/themes/movies2023/Ajaxat/Single/Server.php";
-
     $notListed = [];//[0,2,3,4];
     for ($i = 0; $i < sizeof($servers); $i++) {
         if ( $i == 1 ){
