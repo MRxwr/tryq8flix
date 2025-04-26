54b0c35cf2eea11fbc7cf1e177a4ba0cde12fd2f
MRxwr <nasserhatab@gmail.com>
2025-04-26 03:24:50 +0300

    Update functions.php

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -683,7 +683,7 @@ function TopCenimaListings($url) {
 
 function topCinemaServers($url) {
     GLOBAL $website2;
-    $html = curlCall("{$url}");
+    $html = curlCall("{$url}watch/");
     $dom = str_get_html($html);
     $data = [
         'shows' => []
@@ -695,7 +695,7 @@ function topCinemaServers($url) {
             $jsonData = [
                 'id' => $id,
                 'i' => $i,
-                'link' => "{$url}",
+                'link' => "{$url}watch/",
             ];
             $data['shows'][] = $jsonData;
         }
