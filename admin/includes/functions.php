b3711a3d86e8102eee5c2f2f0f8ff793e18119d5
MRxwr <nasserhatab@gmail.com>
2025-04-26 03:57:02 +0300

    Update functions.php

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -711,8 +711,8 @@ function topCinemaServers($url) {
     for ($i = 0; $i < sizeof($servers); $i++) {
         if (in_array($i, $blackList)) {
         }else{
-            //unset($servers[$i]["link"]);
-            $url1 = makeRequest($ajaxUrl, $servers[$i], str_replace("web2","web5",str_replace("cam","world",$servers[$i]["link"])));
+            unset($servers[$i]["link"]);
+            $url1 = makeRequest($ajaxUrl, $servers[$i], $website2);
             $mainServer[]["link"] = $url1;
         }
     }
