2146c5a83f855610d8b3168b1635e2b3a082b8f3
MRxwr <nasserhatab@gmail.com>
2025-04-26 04:21:22 +0300

    Update functions.php

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -712,7 +712,7 @@ function topCinemaServers($url) {
         if (in_array($i, $blackList)) {
         }else{
             unset($servers[$i]["link"]);
-            $url1 = makeRequest($ajaxUrl, array("data"=>$servers[$i]), $website2);
+            $url1 = makeRequest($ajaxUrl, array("data"=>$servers[$i]), "https://web2.topcinema.cam/");
             $mainServer[]["link"] = $url1;
         }
     }
