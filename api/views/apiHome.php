d227fa053d8c8d844d120f86b319c5068cb84401
MRxwr <nasserhatab@gmail.com>
2025-04-26 04:40:46 +0300

    Update apiHome.php

--- api/views/apiHome.php
+++ api/views/apiHome.php
@@ -47,11 +47,13 @@ if( isset($_GET["action"]) && !empty($_GET["action"]) ){
                 $data = scrapEgyDead("{$url}");
                 echo dataOutput(array("shows" =>$data));die();
             }elseif( $_GET["server"] == 3 ){
-                $url = $website2 . "/recent";
+                $url = $website2;
                 $_GET["page"] = ( isset($_GET["page"]) && !empty($_GET["page"]) ) ? (int)$_GET["page"] + 1 : 1 ;
                 if( isset($_GET["search"]) && !empty($_GET["search"]) ){
                     $_GET["search"] = str_replace(" ","+",$_GET["search"]);
                     $url .= "search/?query={$_GET["search"]}&type=all";
+                }else{
+                    $url .= "/recent";
                 }
                 if( (!isset($_GET["search"]) || empty($_GET["search"])) ){
                     $url .= "/page/{$_GET["page"]}";
