69e79fe2f68101d2c92555708987a8c8a971e573
MRxwr <nasserhatab@gmail.com>
2025-05-13 01:49:20 +0300

    Update functions.php

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -518,7 +518,7 @@ function searchShahidListing($url){
 	$collection = ( isset($_GET["collection"]) ) ? "?order={$_GET["collection"]}" : "" ;
 	$category = ( isset($_GET["category"]) ) ? "&category={$_GET["category"]}" : "" ;
 	$html = scrapePage($url.$collection.$category);
-    var_dump($html); die();
+    //var_dump($html); die();
 	$dom = str_get_html($html);
 	$data = [
 		'shows' => []
