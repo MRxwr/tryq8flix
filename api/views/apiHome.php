77046eecd6c053a77ad6e360375d0ac616b7062d
MRxwr <nasserhatab@gmail.com>
2025-05-13 02:01:21 +0300

    Update apiHome.php

--- api/views/apiHome.php
+++ api/views/apiHome.php
@@ -64,7 +64,7 @@ if( isset($_GET["action"]) && !empty($_GET["action"]) ){
                 echo dataOutput(array("shows" =>$data));die();
             }elseif( $_GET["server"] == 4 ) {
                 $url = $website;
-                if( isset($_GET["search"]) && !empty($_GET["search"]) ){
+                if( isset($_GET["search"]) && !empty($_GET["search"]) && (!isset($_GET["page"]) || empty($_GET["page"])) ){
                     $_GET["search"] = str_replace(" ","+",$_GET["search"]);
                     $url .= "search?s={$_GET["search"]}";
                 }
