f85e82b0858c97f1ee487ad7ee0ecc982087bb29
MRxwr <nasserhatab@gmail.com>
2025-05-22 02:10:15 +0300

    Update functions.php

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -742,7 +742,10 @@ function topCinemaServers($url) {
 }
 
 function scrapEgyDead($url) {
-	$html = file_get_contents(str_replace(" ", "", $url));
+    $url = str_replace(" ", "", $url);
+	$html = file_get_contents($url);
+    echo $url;
+    echo $html; die();
 	$dom = str_get_html($html);
 	$mainSection = $dom->find('.main-section', 0);
 	if (strpos($url, 'category') !== false) {
