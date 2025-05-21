928ee95ff8d18ca621953006e431af6620ffb1d3
MRxwr <nasserhatab@gmail.com>
2025-05-22 02:07:05 +0300

    Update functions.php

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -497,11 +497,10 @@ function curlCall($url) {
 	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
 	curl_setopt($ch, CURLOPT_TIMEOUT, 60);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-        "accept: application/json",
-        "accept-language: ar,en-US;q=0.9,en;q=0.8",
-        "cache-control: no-cache",
-        "pragma: no-cache",
-        "referer: {$url}",
+        "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-language: en-US,en;q=0.9,ar-EG;q=0.8,ar;q=0.7",
+        'cookie: _ga=GA1.1.442917692.1745621347; _ga_Q2XZ3ZSWDY=GS2.1.s1747868306$o7$g1$t1747868513$j0$l0$h0',
+        ':authority: ze0shqhjbe.sbs'
     ));
 	$response = curl_exec($ch);
 	return $response;
