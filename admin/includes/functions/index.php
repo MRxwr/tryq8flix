f9204835e09c1b9bf1e5d794dee47e3564b5d705
MRxwr <nasserhatab@gmail.com>
2025-07-04 02:24:15 +0300

    Update index.php

--- admin/includes/functions/index.php
+++ admin/includes/functions/index.php
@@ -4,7 +4,7 @@ use function vierbergenlars\SemVer\Internal\valid;
 require_once "general.php";
 require_once "sql.php";
 require_once "shahid.php";
-//require_once "edydead.php";
+require_once "egydead.php";
 require_once "topcima.php";
 require_once "wecima.php";
 require_once "notification.php";
