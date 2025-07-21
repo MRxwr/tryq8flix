0f1c33400f81f307121ee9906bf209f85bc7277d
MRxwr <nasserhatab@gmail.com>
2025-07-22 01:37:06 +0300

    savce

--- videoPlayer.php
+++ videoPlayer.php
@@ -7,7 +7,6 @@
 </head>
 <body style="background-color: #1A1A1A;margin: auto;">
     <?php 
-    echo $_GET["link"];
     if (isset($_GET["server"]) && $_GET["server"] != 1 ){
         echo "<iframe id='frame' src='{$_GET["link"]}' style='width:100%;height:100vh;border: none;overflow: hidden;'allowFullScreen></iframe>"; 
     }else{
