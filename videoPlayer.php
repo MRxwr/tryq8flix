cc7cfb3984637c0c7377fbd3aa9f21c42c0e5915
MRxwr <nasserhatab@gmail.com>
2025-07-22 01:34:29 +0300

    Update videoPlayer.php

--- videoPlayer.php
+++ videoPlayer.php
@@ -1,3 +1,19 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adaptive Video Player</title>
+    <script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>
+</head>
+<body style="background-color: #1A1A1A;margin: auto;">
+    <?php 
+    echo $_GET["link"];
+    if (isset($_GET["server"]) && $_GET["server"] != 1 ){
+        echo "<iframe id='frame' src='{$_GET["link"]}' style='width:100%;height:100vh;border: none;overflow: hidden;'allowFullScreen></iframe>"; 
+    }else{
+        echo "<video id='videoPlayer' controls style='width:100%;height:100vh'></video>";
+    }
+    ?>
 <?php 
 require("admin/includes/config.php");
 require("admin/includes/functions.php");
@@ -15,7 +31,6 @@ function getUrlBase($url) {
 }
 
 if( isset($_GET["link"]) && !empty($_GET["link"]) ){
-        $_GET["link"] = urldecode($_GET["link"]);
         $curl = curl_init();
         curl_setopt_array($curl, array(
         CURLOPT_URL => "{$_GET["link"]}",
@@ -41,21 +56,7 @@ if( isset($_GET["link"]) && !empty($_GET["link"]) ){
     echo "لا يوجد روابط متاحه للمشاهده حاليا، الرجاء المحاولة لاحقاً";
 }
 ?>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Adaptive Video Player</title>
-    <script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>
-</head>
-<body style="background-color: #1A1A1A;margin: auto;">
-    <?php 
-    if (isset($_GET["server"]) && $_GET["server"] != 1 ){
-        echo "<iframe id='frame' src='{$_GET["link"]}' style='width:100%;height:100vh;border: none;overflow: hidden;'allowFullScreen></iframe>"; 
-    }else{
-        echo "<video id='videoPlayer' controls style='width:100%;height:100vh'></video>";
-    }
-    ?>
+
 <script>
         function setupVideoPlayer(videoElement, sourceUrl) {
             if (sourceUrl.includes('.m3u8')) {
