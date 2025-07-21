c0399b67ba181ac61ad3c6889406eb35ed28f3d7
MRxwr <nasserhatab@gmail.com>
2025-07-22 01:27:32 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+// Prevent browser caching
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+header("Expires: 0");
 require("admin/includes/config.php");
 require("admin/includes/functions.php");
 $profileData = checkLogin();
@@ -24,7 +29,7 @@ $xValue = md5(time());
     <link rel="apple-touch-icon" href="https://i.imgur.com/6CBCStr.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css?<?php echo randomLetter() . "=" . md5(time()) ?>">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
@@ -32,78 +37,78 @@ $xValue = md5(time());
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
     <script src="js/main.js?<?php echo randomLetter() . "=" . md5(time()) ?>"></script>
     <script id='checkJs' src="<?php echo $js . "?" . randomLetter() . "=" . md5(time()) ?>"></script>
-	<style>
-	body{
-		background-color: #101010;
-	}
-	.card{
-		background-color: #211f20 ;
-		color: white;
-	}
-	header{
-		background-color: #211f20 !important;
-		border: 1px solid black !important;
-		color: black;
-	}
-	.modal-content{
-		background-color: #211f20 ;
-		color: white;
-	}
-	.active-btn {
+    <style>
+    body{
+        background-color: #101010;
+    }
+    .card{
+        background-color: #211f20 ;
+        color: white;
+    }
+    header{
+        background-color: #211f20 !important;
+        border: 1px solid black !important;
+        color: black;
+    }
+    .modal-content{
+        background-color: #211f20 ;
+        color: white;
+    }
+    .active-btn {
     outline: 3px dashed white !important;
     outline-offset: 3px;
     position: relative;
     z-index: 1;
 }
 
-	</style>
+    </style>
 </head>
 
 <body class="container-flex m-0 p-0" <?php if (isset($_GET['js'])) { ?>  onload='bodyLoad();' <?php } ?> style="margin:auto;text-align: -webkit-center;">
-	<div style="max-width:1300px;margin: auto;">
+    <div style="max-width:1300px;margin: auto;">
     <?php require("templates/header.php"); ?>
     <?php //require("templates/navbar.php"); ?> 
-	    <div id="loading-screen" style="display:none">
+        <div id="loading-screen" style="display:none">
             <div class="spinner"></div>
         </div>
-		<?php
-		echo "<div class='row m-0 p-0 w-100'> 
-				<div class='col p-1'>
-				<button id='{$website}' class='btn btn-danger changeIframeSrc w-100 nextBtn'>S4U</button>
-				</div>
-				<div class='col p-1'>
-				<button id='https://www.tuktukcima.com/' class='btn btn-dark changeIframeSrc w-100 nextBtn'>Tuk</button>
-				</div>
-				<div class='col p-1'>
-				<button id='https://e.3sk.media/' class='btn btn-danger changeIframeSrc w-100 nextBtn'>3SQ</button>
-				</div>
-				<div class='col p-1'>
-				<button id='https://bollyrulez.info/' class='btn btn-dark changeIframeSrc w-100 nextBtn'>WWE</button>
-				</div>
-				<div class='col p-1'>
-				<button id='https://anime4up.ch/' class='btn btn-danger changeIframeSrc w-100 nextBtn'>Anime</button>
-				</div>
-			  </div>
-			";
-		?> 
+        <?php
+        echo "<div class='row m-0 p-0 w-100'> 
+                <div class='col p-1'>
+                <button id='{$website}' class='btn btn-danger changeIframeSrc w-100 nextBtn'>S4U</button>
+                </div>
+                <div class='col p-1'>
+                <button id='https://www.tuktukcima.com/' class='btn btn-dark changeIframeSrc w-100 nextBtn'>Tuk</button>
+                </div>
+                <div class='col p-1'>
+                <button id='https://e.3sk.media/' class='btn btn-danger changeIframeSrc w-100 nextBtn'>3SQ</button>
+                </div>
+                <div class='col p-1'>
+                <button id='https://bollyrulez.info/' class='btn btn-dark changeIframeSrc w-100 nextBtn'>WWE</button>
+                </div>
+                <div class='col p-1'>
+                <button id='https://anime4up.ch/' class='btn btn-danger changeIframeSrc w-100 nextBtn'>Anime</button>
+                </div>
+              </div>
+            ";
+        ?> 
     <div id="mainBody">
-		<div class="row m-0 p-o w-100">
-			<div class="col-12 p-3">
-				<button class="btn btn-warning rounded scrapBtn w-100 p-5 nextBtn" id="<?php echo urlencode("js/js3.js") ?>">Server 1 [WeCima]</button>
-			</div>
-			<div class="col-12 p-3">
-				<button class="btn btn-warning rounded scrapBtn w-100 p-5 nextBtn" id="<?php echo urlencode("js/js4.js") ?>">Server 2 [EgyDead]</button>
-			</div>
-			<div class="col-12 p-3">
-				<button class="btn btn-warning rounded scrapBtn w-100 p-5 nextBtn" id="<?php echo urlencode("js/js2.js") ?>">Server 3 [TopCenima]</button>
-			</div>
-			<div class="col-12 p-3">
-				<button class="btn btn-warning rounded scrapBtn w-100 p-5 nextBtn" id="<?php echo urlencode("js/js.js") ?>">Server 4 [Shahid4u]</button>
-			</div>
+        <div class="row m-0 p-o w-100">
             <div class="col-12 p-3">
-				<button class="btn btn-warning rounded scrapBtn w-100 p-5 nextBtn" id="<?php echo urlencode("js/js5.js") ?>">Server 5 [Shahid4uSpace]</button>
-			</div>
-		</div>
+                <button class="btn btn-warning rounded scrapBtn w-100 p-5 nextBtn" id="<?php echo urlencode("js/js3.js") ?>">Server 1 [WeCima]</button>
+            </div>
+            <div class="col-12 p-3">
+                <button class="btn btn-warning rounded scrapBtn w-100 p-5 nextBtn" id="<?php echo urlencode("js/js4.js") ?>">Server 2 [EgyDead]</button>
+            </div>
+            <div class="col-12 p-3">
+                <button class="btn btn-warning rounded scrapBtn w-100 p-5 nextBtn" id="<?php echo urlencode("js/js2.js") ?>">Server 3 [TopCenima]</button>
+            </div>
+            <div class="col-12 p-3">
+                <button class="btn btn-warning rounded scrapBtn w-100 p-5 nextBtn" id="<?php echo urlencode("js/js.js") ?>">Server 4 [Shahid4u]</button>
+            </div>
+            <div class="col-12 p-3">
+                <button class="btn btn-warning rounded scrapBtn w-100 p-5 nextBtn" id="<?php echo urlencode("js/js5.js") ?>">Server 5 [Shahid4uSpace]</button>
+            </div>
+        </div>
         <?php require("templates/content.php"); ?>
     </div>
     <?php //require("templates/footer.php"); ?>
@@ -114,16 +119,16 @@ $xValue = md5(time());
     </div>
     
 <script>
-	// use jquery to listen to a click event and then change the #checkJs src depnding on btn id
-	$(document).on('click', '.scrapBtn', function() {
-		var btnId = $(this).attr('id');
-		$('#checkJs').attr('src', btnId);
-		// refresh page and send $_GET["js"] = btnId
-		window.location.href = "?js=" + btnId;
-		$("#loading-screen").show();
-	});
+    // use jquery to listen to a click event and then change the #checkJs src depnding on btn id
+    $(document).on('click', '.scrapBtn', function() {
+        var btnId = $(this).attr('id');
+        $('#checkJs').attr('src', btnId);
+        // refresh page and send $_GET["js"] = btnId
+        window.location.href = "?js=" + btnId;
+        $("#loading-screen").show();
+    });
 /*
-	$(document).ready(function() {
+    $(document).ready(function() {
     let currentIndex = 0;
     
     // Use event delegation for dynamically loaded content
