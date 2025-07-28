1e982daa77c33519ab5555c3ce8c3870a434b584
MRxwr <nasserhatab@gmail.com>
2025-07-29 01:23:36 +0300

    Update videoPlayer.php

--- videoPlayer.php
+++ videoPlayer.php
@@ -8,7 +8,7 @@
 <body style="background-color: #1A1A1A;margin: auto;">
     <?php 
     if (isset($_GET["server"]) && $_GET["server"] != 1 ){
-        echo "<iframe id='frame' src='{$_GET["link"]}' style='width:100%;height:100vh;border: none;overflow: hidden;'allowFullScreen></iframe>"; 
+        echo "<iframe id='frame' src='{$_GET["link"]}' style='width:100%;height:100vh;border: none;overflow: hidden;'allowFullScreen sandbox='allow-same-origin allow-scripts allow-popups allow-presentation allow-top-navigation'></iframe>"; 
     }else{
         echo "<video id='videoPlayer' controls style='width:100%;height:100vh'></video>";
     }
