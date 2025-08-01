b3b800d9590c01f73b0e2b38fd224b939b02ab16
MRxwr <nasserhatab@gmail.com>
2025-08-01 23:44:20 +0300

    Update config.php

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -18,7 +18,7 @@ $website = "https://shahid4u.mx/";//"https://shaeid4u.net/";//"https://shahid4u.
 $website2 = "https://web5.topcinema.world";
 $website3 = "https://wecima.watch/";//"https://weciema.org/";//"https://wecima.watch/";
 $website4 = "https://ze0shqhjbe.sbs/";
-$website5 = "https://shahid4u.space/";
+$website5 = "https://shahid4u.blog/";
 $websiteLive = "https://yala-shoot-tv.live/";
 $scrappingBeeToken = "0K5RT5UBE82PSAHTQGPJW0XSFE4AR92XXK9YCXF9H0VHSUZT0P2XJQCDOO2N60S332YVUSRD5T2NDOM3";
 //"Y0U1YZ5TQUH84HVVU2U31E6II1D776MU2XAMLCGMDEF0YXCD0CWK4PLMG0S6FI0O5ZDILNYE63W3SWW4";
