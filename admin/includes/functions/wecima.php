64e5ef3e2d5c1a70e42089eb8d3a10c9baff88e7
MRxwr <nasserhatab@gmail.com>
2025-09-20 00:38:23 +0300

    Update wecima.php

--- admin/includes/functions/wecima.php
+++ admin/includes/functions/wecima.php
@@ -41,6 +41,7 @@ function wecimaListing($url) {
 }
 function scrapeWecimaServers($url) {
     $html = curlCall("{$url}");
+    var_dump($html);
     $dom = str_get_html($html);
     $data = [
         'shows' => []
