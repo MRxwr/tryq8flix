2b233f563a836e036c9da27e4d8536a29645472f
MRxwr <nasserhatab@gmail.com>
2025-09-20 01:04:38 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
-include 'admin/includes/config.php';
-include 'admin/includes/functions.php';
-
+include_once('admin/includes/config.php');
+include_once('admin/includes/functions.php');
+/*
 var_dump(curlCall($_GET['url']));
-
+**/
 ?>
