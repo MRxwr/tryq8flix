8b8eaf64e346110ea8f9a0176cbbf3d59a939ab7
MRxwr <nasserhatab@gmail.com>
2025-09-21 01:04:06 +0300

    Update general.php

--- admin/includes/functions/general.php
+++ admin/includes/functions/general.php
@@ -107,7 +107,6 @@ function scrapePage($url) {
 }
 
 function curlCall($url) {
-	var_dump($url. "\n" . $_SERVER['HTTP_USER_AGENT']);
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
