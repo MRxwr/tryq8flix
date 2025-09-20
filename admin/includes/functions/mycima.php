33c56f996c44321f00aecb20037e3087bb3fd952
MRxwr <nasserhatab@gmail.com>
2025-09-20 22:59:56 +0300

    Update mycima.php

--- admin/includes/functions/mycima.php
+++ admin/includes/functions/mycima.php
@@ -2,7 +2,6 @@
 function myCimaHome($url) {
     $url = trim($url);
     $url = str_replace(' ', '+', $url);
-    echo "Fetching URL: " . htmlspecialchars($url) . "<br>";
     $html = curlCall($url);
     $dom = str_get_html($html);
     $data = [
