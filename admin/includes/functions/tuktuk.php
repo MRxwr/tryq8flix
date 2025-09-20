5756aac06f0aac27f2f2e5988c35c4fb81f7bc91
MRxwr <nasserhatab@gmail.com>
2025-09-21 00:36:00 +0300

    Update tuktuk.php

--- admin/includes/functions/tuktuk.php
+++ admin/includes/functions/tuktuk.php
@@ -2,6 +2,7 @@
 function tuktukHome($url) {
     $url = trim($url);
     $url = str_replace(' ', '+', $url);
+    var_dump($url);
     $html = curlCall($url);
     $dom = str_get_html($html);
     $data = [
