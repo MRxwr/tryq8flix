7eb4176d1b6a4f980e885bd4573b371ab84c5956
MRxwr <nasserhatab@gmail.com>
2025-09-20 22:41:29 +0300

    Update test.php

--- test.php
+++ test.php
@@ -2,6 +2,22 @@
 include_once('admin/includes/config.php');
 include_once('admin/includes/functions.php');
 
-var_dump(makeRequest($_GET['url'], '', $website7));
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://mycima.surf/?s=one+piece',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'GET',
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
 
 ?>
