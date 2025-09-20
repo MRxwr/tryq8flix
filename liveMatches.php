dba9e73240b3a177a961519bb72c12828be4829e
MRxwr <nasserhatab@gmail.com>
2025-09-20 18:29:25 +0300

    Update liveMatches.php

--- liveMatches.php
+++ liveMatches.php
@@ -1,7 +1,7 @@
 <?php
 // liveMatches.php
 // Fetch live match links from the API
-$apiUrl = 'api/?endpoint=Live?action=match&match=' . urlencode($_GET['match'] ?? '');
+$apiUrl = 'http://tryq8flix.com/api/?endpoint=Live?action=match&match=' . urlencode($_GET['match'] ?? '');
 $matches = [];
 if (isset($_GET['match'])) {
     $json = file_get_contents($apiUrl);
