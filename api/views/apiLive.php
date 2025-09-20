6644f72b14f9133d75c7d668ed8ad2d35376670f
MRxwr <nasserhatab@gmail.com>
2025-09-20 17:30:27 +0300

    Update apiLive.php

--- api/views/apiLive.php
+++ api/views/apiLive.php
@@ -62,7 +62,7 @@ function liveMatch($view) {
 		foreach ($dom->find('iframe') as $iframe) {
 			if ($iframe) { 
 				$jsonData = [
-					'src' => $iframe->getAttribute('src'),
+					'src' => $iframe->getAttribute('src') . "index.php",
 				];
 				$data['matches'][] = $jsonData;
 			}
