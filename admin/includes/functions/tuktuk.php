aae6985a8073132e9ce94a14afa06057913d9d6b
MRxwr <nasserhatab@gmail.com>
2025-09-21 01:08:34 +0300

    Update tuktuk.php

--- admin/includes/functions/tuktuk.php
+++ admin/includes/functions/tuktuk.php
@@ -144,7 +144,7 @@ function tuktukServers($url) {
             }
             $servers[] = [
                 'name' => $name,
-                'link' => "https://tryq8flix.com/videoPlayer.php?link=" . $decodedLink
+                'link' => $decodedLink
             ];
         }
         $dom->clear();
