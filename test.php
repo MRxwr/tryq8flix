1b0d2c55576938596f973027730b6d7c1708893e
MRxwr <nasserhatab@gmail.com>
2025-09-20 22:47:02 +0300

    Update test.php

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 include_once('admin/includes/config.php');
 include_once('admin/includes/functions.php');
 $url = urlencode($_GET['url']);
-echo $url;
+echo urldecode($url);
 var_dump(curlCall($url));
 
 ?>
