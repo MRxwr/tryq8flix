8d75b592667e19966a7c95d5ba8b2150d6d86bc3
MRxwr <nasserhatab@gmail.com>
2025-09-20 17:36:38 +0300

    Update apiLive.php

--- api/views/apiLive.php
+++ api/views/apiLive.php
@@ -74,7 +74,7 @@ function liveMatch($view) {
 				$jsonData = [
 					'src' => $finalUrl
 				];
-				$data['matches'][] = $jsonData;
+				$data['matches'][] = "https:" . $jsonData;
 			}
 		}
 		$matches = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
