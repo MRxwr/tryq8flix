a8b52c24ec8892b09077a1f4786c75f8dedd9f0a
MRxwr <nasserhatab@gmail.com>
2025-11-04 22:58:39 -0500

    Update wecima.php

--- admin/includes/functions/wecima.php
+++ admin/includes/functions/wecima.php
@@ -69,6 +69,9 @@ function scrapeWecimaServers($url) {
 function scrapeWecima($url) {
     GLOBAL $website3;
     $url = ( !isset($url) || empty($url) ) ? $website3 : $url;
+    if( !file_get_contents($url) ){
+        return 0;
+    }
     $html = file_get_contents($url);
     $dom = str_get_html($html);
     if ($dom) {
