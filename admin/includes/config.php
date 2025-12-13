d902abaadc5088dfde05685464e6ff3bd4162c9d
MRxwr <nasserhatab@gmail.com>
2025-12-14 00:35:03 +0300

    Update config.php

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -14,14 +14,14 @@ if (!$dbconnect) {
 }
 date_default_timezone_set('Asia/Kuwait');
 $date = date('Y-m-d H:i:s');
-$website = "https://shhahid4u.diy/";
-$website2 = "https://web6.topcinema.cloud/";
-$website3 = "https://wecima.ac/we/";
+$website = "https://shaheid4u.day/";
+$website2 = "https://web6.topcinema.cloud/home13/";
+$website3 = "https://wecima.click/we/";
 $website4 = "https://a.a5s0d.sbs/";
-$website5 = "https://shahiid4u.live/";
-$website6 = "https://vid.shahidwbas.tv/";
-$website7 = "https://topcinema.surf/";
-$website8 = "https://tuk.tuktukarab.cfd/";
+$website5 = "https://shaheid4u.live/";
+$website6 = "https://w6.shahidwbas.tv/";
+$website7 = "https://topcinema.zone/";
+$website8 = "https://tuk.tuktukarab.cfd/"; //"https://tuk.tuktukarab.cfd/main/";
 $websiteLive = "https://yala-shoot-tv.live/";
 $scrappingBeeToken = "0K5RT5UBE82PSAHTQGPJW0XSFE4AR92XXK9YCXF9H0VHSUZT0P2XJQCDOO2N60S332YVUSRD5T2NDOM3";
 ?>
