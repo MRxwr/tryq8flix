e7f0329c9579682f349cad3db50ebf361baa3717
MRxwr <nasserhatab@gmail.com>
2025-12-14 01:22:00 +0300

    Update wecima.php

--- admin/includes/functions/wecima.php
+++ admin/includes/functions/wecima.php
@@ -49,7 +49,7 @@ function scrapeWecimaServers($url) {
     $data = [ 'shows' => [] ];
     if ($dom) {
         foreach ($dom->find('.WatchServersList li btn') as $btn) {
-            $encoded = $btn->getAttribute('data-url');
+            echo $encoded = $btn->getAttribute('data-url');
             if ($encoded) {
                 $encoded = str_replace('+', '', $encoded);
                 $decoded = base64_decode($encoded);
