d795f7dcdd88a8d7ffb012c7197d6dfbf6f6ad30
MRxwr <nasserhatab@gmail.com>
2025-12-14 03:12:11 +0300

    Update shahidwbs.php

--- admin/includes/functions/shahidwbs.php
+++ admin/includes/functions/shahidwbs.php
@@ -114,8 +114,28 @@ function scrapeShahidwBsServers($url) {
     return $data['shows'];
 }
 function scrapeShahidwBs($url) {
-    $html = file_get_contents($url);
-    echo $html;
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_HTTPHEADER => array(
+            'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+            'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language: en-US,en;q=0.9',
+            'Referer: https://w6.shahidwbas.tv',
+            'Origin: https://w6.shahidwbas.tv',
+            'X-Requested-With: XMLHttpRequest'
+        ),
+    ));
+    $html = curl_exec($curl);
+    curl_close($curl);
     $dom = str_get_html($html);
     if ($dom) {
         $data = [
