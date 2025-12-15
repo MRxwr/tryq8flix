0599d70c8abba6d85abc4fe145f1fc7776cd5ab9
MRxwr <nasserhatab@gmail.com>
2025-12-16 00:32:03 +0300

    save

--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,20 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script>
+    function getCookie(name) {
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${name}=`);
+        if (parts.length === 2) return parts.pop().split(';').shift();
+        return '';
+    }
+
+    $(document).ajaxSend(function(e, xhr, options) {
+        const token = getCookie('tryq8flix2');
+        if (token) {
+            xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+        }
+    });
+
     $(window).scroll(function() {
         if ($(this).scrollTop() > 50) {
             $('.navbar').addClass('scrolled');
