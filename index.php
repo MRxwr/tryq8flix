26bcf4cd7e92707f1355734ca9b7fc5ed83a562f
MRxwr <nasserhatab@gmail.com>
2025-12-16 01:08:13 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,23 @@ require_once("admin/includes/config.php");
 require_once("admin/includes/functions.php");
 require_once("try2/templates/simple_html_dom.php");
 
+$profileData = checkLogin();
+$view = isset($_GET["v"]) ? $_GET["v"] : "Home";
+
+// Whitelist of views accessible without login
+$publicViews = ["Login", "Forget"];
+
+if (empty($profileData['id']) && !in_array($view, $publicViews)) {
+    header("Location: ?v=Login");
+    exit();
+}
+
+// Optional: Redirect logged-in users away from Login page
+if (!empty($profileData['id']) && $view == "Login") {
+    header("Location: ?v=Home");
+    exit();
+}
+
 // get viewed page from pages folder \\
 if( isset($_GET["v"]) && file_exists("views/blade{$_GET["v"]}.php") ){
 	require_once("views/blade{$_GET["v"]}.php");
