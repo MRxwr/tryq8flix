5d32af51f6a304f2434a970ddb61aae2f15bf117
MRxwr <nasserhatab@gmail.com>
2025-12-16 17:37:23 +0300

    Update apiLive.php

--- api/views/apiLive.php
+++ api/views/apiLive.php
@@ -1,7 +1,21 @@
 <?php
+function liveCurl($url) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    return $response;
+}
+
 function searchMatches() {
 	GLOBAL $websiteLive;
-	$html = curlCall("{$websiteLive}");
+	$html = liveCurl("{$websiteLive}");
     $dom = str_get_html($html);
     if ($dom) {
         $data = [
@@ -53,7 +67,7 @@ function searchMatches() {
 }
 
 function liveMatch($view) {
-	$html = curlCall("{$view}");
+	$html = liveCurl("{$view}");
     $dom = str_get_html($html);
 	if ($dom) {
 		$data = [
@@ -64,7 +78,7 @@ function liveMatch($view) {
 				$baseSrc = $iframe->getAttribute('src');
 				for ($serv = 1; $serv <= 6; $serv++) {
 					$srcWithIndex = $baseSrc . "index.php?serv=" . $serv;
-					$iframeHtml = curlCall($srcWithIndex);
+					$iframeHtml = liveCurl($srcWithIndex);
 					$iframeDom = str_get_html($iframeHtml);
 					if ($iframeDom) {
 						$foundIframe = $iframeDom->find('iframe', 0);
