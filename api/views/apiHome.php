4004c4d3f4a6504637db407d1b4e200cd33d46b6
MRxwr <nasserhatab@gmail.com>
2025-12-16 21:40:04 +0300

    Update apiHome.php

--- api/views/apiHome.php
+++ api/views/apiHome.php
@@ -26,12 +26,16 @@ if( isset($_GET["action"]) && !empty($_GET["action"]) ){
                 $url = $website4;
                 //strip last /
                 $url = rtrim($url, '/');
-                if( isset($_GET["page"]) && !empty($_GET["page"])){
+                if( isset($_GET["page"]) && !empty($_GET["page"]) && (!isset($_GET["search"]) || empty($_GET["search"])) ){
                     $url .= "/?page={$_GET["page"]}/";
                 }
-                if( isset($_GET["search"]) && !empty($_GET["search"]) ){
+                if( isset($_GET["search"]) && !empty($_GET["search"]) && (!isset($_GET["page"]) || empty($_GET["page"])) ){
                     $_GET["search"] = urlencode($_GET["search"]);;
                     $url .= "/?s={$_GET["search"]}";
+                }
+                if( isset($_GET["page"]) && !empty($_GET["page"]) && (isset($_GET["search"]) && empty($_GET["search"])) ){
+                    $_GET["search"] = urlencode($_GET["search"]);
+                    $url .= "/page/{$_GET["page"]}/?s={$_GET["search"]}";
                 }
                 $data = scrapEgyDead("{$url}");
                 echo dataOutput(array("shows" =>$data));die();
