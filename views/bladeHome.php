4a1d41a5b6391d568650b1335ca52eb4dab75e76
MRxwr <nasserhatab@gmail.com>
2025-12-16 16:44:33 +0300

    Update bladeHome.php

--- views/bladeHome.php
+++ views/bladeHome.php
@@ -92,8 +92,7 @@ $(document).ready(function() {
             if(data.servers && data.servers.length > 0) {
                 data.servers.forEach(server => {
                     // Fetch content for each server
-                    $.getJSON('api/index.php?endpoint=Home&action=view&server=' + server.id, function(serverRes) {
-                        console.log('Server Response:', server.name, serverRes);
+                    $.getJSON('api/index.php?endpoint=Home&action=view&page=1&server=' + server.id, function(serverRes) {
                         if(serverRes.ok && serverRes.data.shows && serverRes.data.shows.length > 0) {
                             const rowId = `row-${server.id}`;
                             let rowHtml = `
