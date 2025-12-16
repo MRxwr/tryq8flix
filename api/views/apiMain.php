eae797fac3d837fc37c20c3524d10bbe40ead646
MRxwr <nasserhatab@gmail.com>
2025-12-17 02:05:11 +0300

    Update apiMain.php

--- api/views/apiMain.php
+++ api/views/apiMain.php
@@ -6,6 +6,22 @@ $response = array(
             "name" => "Wecima",
         ),
         array(
+            "id" => 7, 
+            "name" => "Top Cinema Zone",
+        ),
+        array(
+            "id" => 5,
+            "name" => "Shahid Space",
+        ),
+        array(
+            "id" => 8,
+            "name" => "TukTuk",
+        ),
+        array(
+            "id" => 4,
+            "name" => "Shahid",
+        ),
+        array(
             "id" => 2,
             "name" => "EgyDead",
         ),
@@ -14,24 +30,8 @@ $response = array(
             "name" => "TopCinema",
         ),
         array(
-            "id" => 4,
-            "name" => "Shahid",
-        ),
-        array(
-            "id" => 5,
-            "name" => "Shahid Space",
-        ),
-        array(
             "id" => 6,
             "name" => "ShahidwBs",
-        ),
-        array(
-            "id" => 7, 
-            "name" => "Top Cinema Zone",
-        ),
-        array(
-            "id" => 8,
-            "name" => "TukTuk",
         ),
     )
 );
