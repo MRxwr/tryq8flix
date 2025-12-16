56404a5aeb87b068771657a0a02c46418ea87820
MRxwr <nasserhatab@gmail.com>
2025-12-16 21:36:31 +0300

    Update apiHome.php

--- api/views/apiHome.php
+++ api/views/apiHome.php
@@ -27,7 +27,7 @@ if( isset($_GET["action"]) && !empty($_GET["action"]) ){
                 //strip last /
                 $url = rtrim($url, '/');
                 if( isset($_GET["page"]) && !empty($_GET["page"])){
-                    $url .= "/page/{$_GET["page"]}";
+                    $url .= "/?page={$_GET["page"]}/";
                 }
                 if( isset($_GET["search"]) && !empty($_GET["search"]) ){
                     $_GET["search"] = str_replace(" ","+",$_GET["search"]);
