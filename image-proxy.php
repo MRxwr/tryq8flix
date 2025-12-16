fb8e73d8f842528b6740e411bf5e6f74a25030c0
MRxwr <nasserhatab@gmail.com>
2025-12-17 00:18:04 +0300

    Update image-proxy.php

--- image-proxy.php
+++ image-proxy.php
@@ -19,6 +19,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36');
 curl_setopt($ch, CURLOPT_HEADER, 1);
 
 // Execute cURL session and get the response
