fff23c1d4cdc29a06787304ae0e1aed1364242ce
MRxwr <nasserhatab@gmail.com>
2025-12-19 00:03:58 +0300

    Update animeslayer.php

--- admin/includes/functions/animeslayer.php
+++ admin/includes/functions/animeslayer.php
@@ -22,32 +22,47 @@ function animeSlayerHome($url) {
         'shows' => []
     ];
     if ($dom) {
-        $list = $dom->find('ul.Blocks--List', 0);
-        if ($list) {
-            foreach ($list->find('div.Block--Item') as $item) {
+        $items = $dom->find('div.bsx');
+        if ($items) {
+            foreach ($items as $item) {
                 $a = $item->find('a', 0);
                 $href = $a ? $a->href : '';
+                
                 $img = $item->find('img', 0);
-                $image = $img && $img->getAttribute('data-src') ? $img->getAttribute('data-src') : '';
-                $genres = [];
-                $genresList = $item->find('ul.Genres', 0);
-                if ($genresList) {
-                    foreach ($genresList->find('li') as $li) {
-                        $genres[] = trim($li->plaintext);
+                $image = '';
+                if ($img) {
+                    if ($img->src && strpos($img->src, 'data:image') === false) {
+                        $image = $img->src;
+                    } elseif ($img->getAttribute('data-src')) {
+                        $image = $img->getAttribute('data-src');
                     }
                 }
+
                 $title = '';
-                $h3 = $item->find('h3', 0);
-                if ($h3) {
-                    $title = trim($h3->plaintext);
+                $h2 = $item->find('h2', 0);
+                if ($h2) {
+                    $title = trim($h2->plaintext);
                 }
-                // Keep same array keys, fill missing with empty string/array
-                $posterUrl = 'https://' . $_SERVER['HTTP_HOST'] . '/image-proxy.php?url=' . urlencode(trim($image));
+
+                $category = '';
+                $typez = $item->find('div.typez', 0);
+                if ($typez) {
+                    $category = trim($typez->plaintext);
+                }
+
+                $episode = '';
+                $epx = $item->find('span.epx', 0);
+                if ($epx) {
+                    $episode = trim($epx->plaintext);
+                }
+
+                $genres = [];
+
                 $jsonData = [
                     'href' => $href,
                     'image' => trim($image),
-                    'episode' => '',
-                    'category' => '',
+                    'episode' => $episode,
+                    'category' => $category,
                     'title' => $title,
                     'description' => '',
                     'genres' => $genres
