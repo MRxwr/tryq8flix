6a3044c602462d69890841f99baa70be25d0adab
MRxwr <nasserhatab@gmail.com>
2025-12-20 19:56:11 +0300

    save

--- views/bladeSettings.php
+++ views/bladeSettings.php
@@ -47,6 +47,12 @@
                     <a href="?v=LiveMatchesList" class="list-group-item list-group-item-action bg-dark text-white border-secondary">
                         <i class="fas fa-futbol me-2"></i> Live Matches
                     </a>
+                    <a href="?v=Search" class="list-group-item list-group-item-action bg-dark text-white border-secondary">
+                        <i class="fas fa-search me-2"></i> Search
+                    </a>
+                    <a href="?v=Favorites" class="list-group-item list-group-item-action bg-dark text-white border-secondary">
+                        <i class="fas fa-heart me-2"></i> My Favorites
+                    </a>
                     <a href="?v=History" class="list-group-item list-group-item-action bg-dark text-white border-secondary">
                         <i class="fas fa-history me-2"></i> Watch History
                     </a>
