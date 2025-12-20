d781c98e71bfab7a9955466b4f29fabee16ae710
MRxwr <nasserhatab@gmail.com>
2025-12-20 19:12:36 +0300

    dsbr

--- views/bladeForget.php
+++ views/bladeForget.php
@@ -5,6 +5,7 @@
         <h2 class="mb-4 text-white fw-bold" style="text-shadow: 1px 1px 8px #000, 0 0 2px #e50914;">Forgot Password</h2>
         <p class="text-white">Enter your email address and we'll send you a link to reset your password.</p>
         <form id="forgetForm">
+            <div id="message" class="mb-3"></div>
             <div class="mb-3">
                 <input type="email" class="form-control" id="email" placeholder="name@example.com" required>
             </div>
@@ -22,9 +23,25 @@
 $('#forgetForm').submit(function(e) {
     e.preventDefault();
     const email = $('#email').val();
+    const btn = $(this).find('button[type="submit"]');
+    const originalText = btn.text();
+    
+    btn.prop('disabled', true).text('Sending...');
+    $('#message').html('');
+
     $.post('api/index.php?endpoint=User&action=forget', {email: email}, function(response) {
-        const res = JSON.parse(response);
-        alert(res.data.msg);
+        const res = (typeof response === 'string') ? JSON.parse(response) : response;
+        
+        if(res.ok) {
+            $('#message').html('<div class="alert alert-success">' + res.data.msg + '</div>');
+            $('#email').val('');
+        } else {
+            $('#message').html('<div class="alert alert-danger">' + (res.data.msg || 'An error occurred') + '</div>');
+        }
+    }).fail(function() {
+        $('#message').html('<div class="alert alert-danger">Network error. Please try again.</div>');
+    }).always(function() {
+        btn.prop('disabled', false).text(originalText);
     });
 });
 </script>
