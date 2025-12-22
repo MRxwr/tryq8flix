a78d39d3c0f0bd96a640feb6fb71113b285e9057
MRxwr <nasserhatab@gmail.com>
2025-12-22 23:58:10 +0300

    asve

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -26,6 +26,6 @@ $website9 = "https://qesset.net/";
 $website10 = "https://ser.q-ask.video/";
 $website11 = "https://animeslayerweb.com/";
 $website12 = "https://k3zgx2.animepec.online/"; // anime4up
-$websiteLive = "https://yala-shoot-tv.live/";
+$websiteLive = "https://beinmatch1.live/match"; //"https://yala-shoot-tv.live/";
 $scrappingBeeToken = "0K5RT5UBE82PSAHTQGPJW0XSFE4AR92XXK9YCXF9H0VHSUZT0P2XJQCDOO2N60S332YVUSRD5T2NDOM3";
 ?>
