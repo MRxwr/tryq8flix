a6949b1bf3cf830bf8fd2f3eb18b8858d30f9301
MRxwr <nasserhatab@gmail.com>
2025-12-27 00:20:56 +0300

    Update shahid.php

--- admin/includes/functions/shahid.php
+++ admin/includes/functions/shahid.php
@@ -66,12 +66,12 @@ function searchShahidListing($url){
 			$style = $show->style;
 			preg_match('/\burl\s*\(\s*[\'"]?(.*?)[\'"]?\s*\)/', $style, $matches);
 			$imageUrl = isset($matches[1]) ? $matches[1] : '';
-            /*
+            
             // Use image proxy for all images to avoid CORS/hotlink issues
             if (!empty($imageUrl)) {
                 $imageUrl = "https://".$_SERVER['HTTP_HOST']."/image-proxy.php?url=".urlencode(trim($imageUrl));
             }
-            */
+            
             $href = $show->href;
             $parts = explode('/', $href);
             if( count($parts) >= 5 ){
