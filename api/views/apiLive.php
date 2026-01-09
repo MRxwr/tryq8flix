a194578f8440b7fe6c6b7265ddfc84c40d22eb69
MRxwr <nasserhatab@gmail.com>
2026-01-09 18:48:40 +0300

    Update apiLive.php

--- api/views/apiLive.php
+++ api/views/apiLive.php
@@ -102,8 +102,11 @@ function liveMatch($view) {
 					if (strpos($src, '//') === 0) {
 						$src = 'https:' . $src;
 					}
+					// Wrap the extracted src in videoPlayer.php
+					$videoLnk = "https://tryq8flix.com/videoPlayer.php?link=" . urlencode($src);
+					
 					$data['matches'][] = [
-						'live' => $src,
+						'live' => $videoLnk,
 						'name' => $q['name'],
 						'src' => $q['url']
 					];
