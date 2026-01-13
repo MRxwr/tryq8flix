f2abc54f2c666586f4f61dda70bfb5e236f4942e
MRxwr <nasserhatab@gmail.com>
2026-01-13 16:25:59 +0300

    Update config.php

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -14,14 +14,14 @@ if (!$dbconnect) {
 }
 date_default_timezone_set('Asia/Kuwait');
 $date = date('Y-m-d H:i:s');
-$website = "https://sheid4u.com/"; //"https://shhahhid4u.net/"; //"https://shaheid4u.day/";
+$website = "https://sheid4u.com/";
 $website2 = "https://web6.topcinema.cloud/home13/";
 $website3 = "https://wecima.click/we";
 $website4 = "https://a.a5s0d.sbs/";
 $website5 = "https://shaheid4u.live/";
 $website6 = "https://w6.shahidwbas.tv/";
 $website7 = "https://topcinema.zone/";
-$website8 = "https://tuk.tuktukarab.cfd/"; //"https://tuk.tuktukarab.cfd/main/";
+$website8 = "https://tuk.tuktukarab.cfd/";
 $website9 = "https://qesset.net/";
 $website10 = "https://ser.q-ask.video/";
 $website11 = "https://animeslayerweb.com/";
