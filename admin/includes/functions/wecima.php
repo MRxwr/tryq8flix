2255269b147613443ad2bb18d0d99e1a7d33639e
MRxwr <nasserhatab@gmail.com>
2026-03-23 15:56:37 +0300

    Update wecima.php

--- admin/includes/functions/wecima.php
+++ admin/includes/functions/wecima.php
@@ -213,7 +213,7 @@ function scrapeWecima($url) {
             $link = $thumbDiv ? $thumbDiv->find('a', 0) : null;
             $bgSpan = $item->find('span.BG--GridItem', 0);
             $h2 = $link ? $link->find('.hasyear', 0) : null;
-
+            var_dump($bgSpan);
             // Extract image URL from data-src or style attribute
             $imageUrl = '';
             if ($bgSpan) {
