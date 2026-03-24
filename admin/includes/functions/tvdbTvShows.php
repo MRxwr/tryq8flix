5c1dbdfd950d3542c070caacba4fa2e49ad1a494
MRxwr <nasserhatab@gmail.com>
2026-03-24 15:56:10 +0300

    Update tvdbTvShows.php

--- admin/includes/functions/tvdbTvShows.php
+++ admin/includes/functions/tvdbTvShows.php
@@ -59,9 +59,12 @@ function tvdbTvShowsListings($id) {
         $parts = explode('/', $id);
         $showId = $parts[0];
         $seasonNum = end($parts);
+    } elseif (isset($_GET['href']) && strpos($_GET['href'], 'season/') !== false) {
+        $parts = explode('/', $_GET['href']);
+        $showId = $parts[0];
+        $seasonNum = end($parts);
     }
-    var_dump($showId, $seasonNum);
-
+    
     $token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MGJjZWE4ZDI5YmNlOTkzZjBiZDJjNzVjOGE0OGVjMiIsIm5iZiI6MTcyMDEzMjA4My4yOSwic3ViIjoiNjY4NzIxZjNhMTM1MjQyZWQ5MjY1ZmZhIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.l66VL660inwA8lNw9mzA7RBOvcWuVi_0f0fmw9FfhIM';
     
     $ch = curl_init();
