4bde735cfda918a2a13044d7c4e3fa76f4ccaab9
MRxwr <nasserhatab@gmail.com>
2026-04-15 20:31:35 +0300

    savbe

--- api/views/apiMain.php
+++ api/views/apiMain.php
@@ -3,7 +3,7 @@ $response = array(
     "servers" => array(
         array("id" => 1, "name" => "Wecima"),
         array("id" => 2, "name" => "EgyDead"),
-        array("id" => 3, "name" => "TopCinema" ),
+        //array("id" => 3, "name" => "TopCinema" ),
         //array("id" => 4, "name" => "Shahid"),
         //array("id" => 5, "name" => "Shahid Space"),
         //array("id" => 6, "name" => "ShahidwBs" ),
@@ -11,8 +11,8 @@ $response = array(
         //array("id" => 8, "name" => "TukTuk"), 
         array("id" => 9, "name" => "Qesset"),
         array("id" => 10, "name" => "Esq"),
-        array("id" => 11, "name" => "Anime Slayer"),
-        array("id" => 12, "name" => "Anime Pec"),
+        //array("id" => 11, "name" => "Anime Slayer"),
+        //array("id" => 12, "name" => "Anime Pec"),
         array("id" => 13, "name" => "TVDB TV Shows"),
         array("id" => 14, "name" => "TVDB Movies"),
     )
