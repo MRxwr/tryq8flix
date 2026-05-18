94445c82763ab49ec2a047a1b86d0cffb3abfc36
MRxwr <nasserhatab@gmail.com>
2026-05-19 02:43:05 +0300

    Update paymentStatus.php

--- bookeey/paymentStatus.php
+++ bookeey/paymentStatus.php
@@ -15,8 +15,7 @@ ini_set("display_errors", "1");
 error_reporting(E_ALL);
 include_once("bookeey.php");
 $bookeeyPipe = new bookeey;
-$bookeeyPipe->setIsTestModeEnable(1);          // Enable Test Mode
-$bookeeyPipe->setMerchantID('mer23000173');    // Set the Merchant ID (using test credentials if needed)
+$bookeeyPipe->setMerchantID('mer23000173');    // Set the Merchant ID
 $bookeeyPipe->setSecretKey('4653344');    // Set the Secret Key
  // Pass Transaction Ids in the array
 $orderIds = array(
