ca6e7d4d045ae26873c01de2c6a0f273c65a9eca
MRxwr <nasserhatab@gmail.com>
2026-05-18 15:39:12 +0300

    Update buy.php

--- bookeey/buy.php
+++ bookeey/buy.php
@@ -14,6 +14,7 @@
 error_reporting(E_ALL);
 ini_set("display_errors", "1");
 include_once("bookeey.php");
+echo "<!-- DEBUG: File Path: " . __FILE__ . " -->";
 $bookeeyPipe = new bookeey;
 $isEnable = $bookeeyPipe->isEnable();
 
