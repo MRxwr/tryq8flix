72b0bbfbf89bc11ef9097aed3a177cbaf2bf4e36
MRxwr <nasserhatab@gmail.com>
2026-06-02 08:06:48 +0300

    Update witanime.php

--- admin/includes/functions/witanime.php
+++ admin/includes/functions/witanime.php
@@ -199,7 +199,7 @@ function witanimeListings($url) {
 }
 
 function witanimeServers($url) {
-    echo $url = trim($url);
+    $url = trim($url);
     // Properly encode Arabic/special characters in the URL
     $url_parts = parse_url($url);
     if (isset($url_parts['path'])) {
