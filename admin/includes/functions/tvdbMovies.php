9d658d75cb25b04a4bbe782c9d43b4868d359fd2
MRxwr <nasserhatab@gmail.com>
2026-06-03 04:54:59 +0300

    save

--- admin/includes/functions/tvdbMovies.php
+++ admin/includes/functions/tvdbMovies.php
@@ -69,12 +69,14 @@ function tvdbMoviesServers($id) {
     $date = isset($res['release_date']) ? $res['release_date'] : "";
 
     return [
-        ['name' => 'Server VidKing', 'link' => "https://www.vidking.net/embed/movie/{$id}", 'backdrop' => $backdrop, 'overview' => $overview, 'date' => $date],
-        ['name' => 'Server Vidsrc CC', 'link' => "https://vidsrc.cc/v2/embed/movie/{$id}", 'backdrop' => $backdrop, 'overview' => $overview, 'date' => $date],
-        ['name' => 'Server Vidsrc ME', 'link' => "https://vidsrc.me/embed/movie/{$id}", 'backdrop' => $backdrop, 'overview' => $overview, 'date' => $date],
-        ['name' => 'Server Videasy', 'link' => "https://player.videasy.net/movie/{$id}", 'backdrop' => $backdrop, 'overview' => $overview, 'date' => $date],
-        ['name' => 'Server Peachify', 'link' => "https://peachify.top/embed/movie/{$id}", 'backdrop' => $backdrop, 'overview' => $overview, 'date' => $date],
-        ['name' => 'Server Xpass', 'link' => "https://play.xpass.top/e/movie/{$id}", 'backdrop' => $backdrop, 'overview' => $overview, 'date' => $date]
+        ['name' => 'Server VidKing', 'link' => "https://www.vidking.net/embed/movie/{$id}?autoplay=1", 'backdrop' => $backdrop, 'overview' => $overview, 'date' => $date],
+        ['name' => 'Server Vidsrc CC', 'link' => "https://vidsrc.cc/v2/embed/movie/{$id}?autoplay=1", 'backdrop' => $backdrop, 'overview' => $overview, 'date' => $date],
+        ['name' => 'Server Vidsrc ME', 'link' => "https://vidsrc.me/embed/movie/{$id}?autoplay=1", 'backdrop' => $backdrop, 'overview' => $overview, 'date' => $date],
+        ['name' => 'Server Videasy', 'link' => "https://player.videasy.net/movie/{$id}?autoplay=1", 'backdrop' => $backdrop, 'overview' => $overview, 'date' => $date],
+        ['name' => 'Server Vidcore', 'link' => "https://vidcore.net/movie/{$id}?autoplay=1", 'backdrop' => $backdrop, 'overview' => $overview, 'date' => $date],
+        ['name' => 'Server Vaplayer', 'link' => "https://vaplayer.ru/embed/movie/{$id}?autoplay=1", 'backdrop' => $backdrop, 'overview' => $overview, 'date' => $date],
+        ['name' => 'Server ZXC', 'link' => "https://zxcstream.xyz/player/movie/{$id}?autoplay=true", 'backdrop' => $backdrop, 'overview' => $overview, 'date' => $date],
+        ['name' => 'Server Mapple', 'link' => "https://mapple.uk/watch/movie/{$id}?autoPlay=true", 'backdrop' => $backdrop, 'overview' => $overview, 'date' => $date]
     ];
 }
 
