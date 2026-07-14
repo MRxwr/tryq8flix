776ad1601df7250e7dd122119fcfa72fa799ac11
MRxwr <nasserhatab@gmail.com>
2026-07-14 22:17:22 +0300

    Update config.php

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -34,5 +34,5 @@ $website11 = "https://animeslayerweb.com/";
 $website12 = "https://y0vx70khe8u.animepec.online/"; // anime4up
 $website13 = "https://egydead.lat/";
 $website14 = "https://witanime.you/";
-$websiteLive = "https://yalla-shoot-new.site/";
+$websiteLive = "https://yalla-shoot-8k.net/";
 $websiteLive2 = "https://beinmatch2026.com/";
